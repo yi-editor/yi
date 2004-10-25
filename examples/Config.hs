3cb47daaf61a6590ea5080dd460a657727eff5fe
Don Stewart <dons@cse.unsw.edu.au>
2004-10-25 12:16:26 +0000

    Example of how to split a config file over multiple modules

---
+++ examples/Config.hs
@@ -0,0 +1,16 @@
+-- Copyright (c) 2004 Don Stewart - http://www.cse.unsw.edu.au/~dons
+-- GPL version 2 or later (see http://www.gnu.org/copyleft/gpl.html)
+
+--
+-- An example of how to write a simple top level config module that
+-- pulls in a keymap from another local module.
+--
+
+module Config where
+
+import Yi.Yi
+import qualified Keymap
+
+yi = settings {
+    keymap = Keymap.keymap
+}
