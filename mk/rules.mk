dcfb135418df427cca293cadaeb7b489f8977ec0
Don Stewart <dons@cse.unsw.edu.au>
2004-11-27 08:13:13 +0000

    Misc doc fixes. Turn off -fignore-asserts. Make ghc-pkg quieter

--- mk/rules.mk
+++ mk/rules.mk
@@ -141,13 +141,13 @@ $(GHCI_LIBRARY) : $(LIBOBJS)
 $(PKG).conf: $(PKG).conf.in.cpp
 	@cpp < $(PKG).conf.in.cpp | sed 's/""//g;s/\[ *,/[ /g;/^#/d' > $(PKG).conf.in
 	@if [ ! -f $(PKG).conf ]; then echo [] > $(PKG).conf ; fi
-	@env PREFIX=`pwd` CURSES=$(CURSES) ICONV=$(ICONV) $(GHC_PKG) --force -f $(PKG).conf -u < $(PKG).conf.in
+	@env PREFIX=`pwd` CURSES=$(CURSES) ICONV=$(ICONV) $(GHC_PKG) --force -f $(PKG).conf -u < $(PKG).conf.in > /dev/null 2>&1
 
 # installable package.conf
 $(PKG).conf.install: $(PKG).conf.in.cpp
 	@cpp -DINSTALLING < $(PKG).conf.in.cpp | sed 's/""//g;s/\[ *,/[ /g;/^#/d' > $(PKG).conf.install.in
 	@if [ ! -f $(PKG).conf.install ]; then echo [] > $(PKG).conf.install ; fi
-	@env PREFIX=$(PREFIX) CURSES=$(CURSES) ICONV=$(ICONV) $(GHC_PKG) --force -f $(PKG).conf.install -u < $(PKG).conf.install.in
+	@env PREFIX=$(PREFIX) CURSES=$(CURSES) ICONV=$(ICONV) $(GHC_PKG) --force -f $(PKG).conf.install -u < $(PKG).conf.install.in > /dev/null 2>&1
 
 EXTRA_CLEANS+= $(PKG).conf.install $(PKG).conf $(PKG).conf.in $(PKG).conf.install.in *.old
 
