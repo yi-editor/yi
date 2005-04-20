7bdd66b3d92868c04b35e7948e376da297b35d13
Don Stewart <dons@cse.unsw.edu.au>
2005-04-20 07:52:54 +0000

    Import lambdabot's Map compat module

--- Yi/Map.hs
+++ Yi/Map.hs
@@ -1,6 +1,6 @@
 {-# OPTIONS -cpp #-}
 -- 
--- Copyright (C) 2004-5 Don Stewart - http://www.cse.unsw.edu.au/~dons
+-- Copyright (c) 2005 Don Stewart - http://www.cse.unsw.edu.au/~dons
 -- 
 -- This program is free software; you can redistribute it and/or
 -- modify it under the terms of the GNU General Public License as
@@ -19,30 +19,45 @@
 -- 
 
 --
--- Compatibility code between Data.FiniteMap and Data.Map
--- We use the function names from Data.Map.
+-- | Compatibility code between Data.FiniteMap and Data.Map We use the
+-- function names from Data.Map.
 --
 module Yi.Map (
 #if __GLASGOW_HASKELL__ >= 604
         module Data.Map,
+        addList,
 #else
         Map,
-        empty, 
-        insert, 
-        delete, 
-        lookup, 
-        fromList, 
-        size, 
-        elems, 
+        empty,
+        insert,
+        insertWith,
+        delete,
+        update,
+        lookup,
+        toList,
+        fromList,
+        addList,
+        size,
+        elems,
         singleton,
         member,
         keys,
+        assocs,
+
+        mapWithKey,
+        filterWithKey,
+        foldWithKey,
 #endif
   ) where
 
 import Prelude hiding (lookup)
+
 #if __GLASGOW_HASKELL__ >= 604
 import Data.Map
+
+addList :: (Ord k) => [(k,a)] -> Map k a -> Map k a
+addList l m = union (fromList l) m
+
 #else
 --
 -- compatibility code for deprecated FiniteMap
@@ -50,25 +65,42 @@ import Data.Map
 import Prelude hiding (lookup)
 import qualified Data.FiniteMap as FM
 
-type Map k a = FM.FiniteMap k a
+type Map = FM.FiniteMap
+
+instance Functor (Map k) where
+  fmap = FM.mapFM . const
 
 empty  :: Map k a
 empty  = FM.emptyFM
 
 singleton :: k -> a -> Map k a
-singleton = FM.unitFM 
+singleton = FM.unitFM
 
 insert :: Ord k => k -> a -> Map k a -> Map k a
 insert = \k e m -> FM.addToFM m k e
 
+-- | This function is pure evil. Avoid it if possible.
+insertWith :: Ord k => (a -> a -> a) -> k -> a -> Map k a -> Map k a
+insertWith f k e m = FM.addToFM_C (flip f) m k e
+
 delete :: Ord k => k -> Map k a -> Map k a
 delete = flip FM.delFromFM
+
+update :: Ord k => (a -> Maybe a) -> k -> Map k a -> Map k a
+update f k m = case lookup k m of
+  Nothing -> m
+  Just v  -> case f v of
+    Nothing -> delete k m
+    Just v' -> insert k v' m
 
 lookup :: Ord k => k -> Map k a -> Maybe a
 lookup = flip FM.lookupFM
 
 fromList :: Ord k => [(k,a)] -> Map k a
 fromList = FM.listToFM
+
+toList :: Map k a -> [(k,a)]
+toList = FM.fmToList
 
 size :: Map k a -> Int
 size = FM.sizeFM
@@ -79,7 +111,33 @@ elems = FM.eltsFM
 member :: Ord k => k -> Map k a -> Bool
 member = FM.elemFM
 
-keys  :: Map k a -> [k] 
+keys  :: Map k a -> [k]
 keys = FM.keysFM
+
+assocs :: Map k a -> [(k, a)]
+assocs = FM.fmToList
+
+addList :: (Ord k) => [(k, a)] -> Map k a -> Map k a
+addList = flip FM.addListToFM
+
+-- delListFromFM = \fm keys -> foldl delete fm keys
+
+-- Posted by Gracjan Polak on haskell-cafe@
+-- note that we want the mapping the other way around.
+--
+-- deleteList list map = foldl (flip Data.Map.delete) map list
+-- insertList asclist map = union map (Data.Map.fromList asclist)
+--
+
+mapWithKey :: (k -> a -> b) -> Map k a -> Map k b
+mapWithKey = FM.mapFM
+
+-- map f m == mapFM (const f)
+
+filterWithKey :: Ord k => (k -> a -> Bool) -> Map k a -> Map k a
+filterWithKey = FM.filterFM
+
+foldWithKey :: (k -> a -> b -> b) -> b -> Map k a -> b
+foldWithKey = FM.foldFM
 
 #endif
