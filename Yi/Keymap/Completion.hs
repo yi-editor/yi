56db039d6981abbe76bfd4a8488e986b591cc8dc
Don Stewart <dons@cse.unsw.edu.au>
2005-08-04 02:44:45 +0000

    Bit of haddock and a license

--- Yi/Keymap/Completion.hs
+++ Yi/Keymap/Completion.hs
@@ -1,12 +1,28 @@
+-- 
+-- Copyright (c) B.Zapf July 2005
+-- 
+-- This program is free software; you can redistribute it and/or
+-- modify it under the terms of the GNU General Public License as
+-- published by the Free Software Foundation; either version 2 of
+-- the License, or (at your option) any later version.
+-- 
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+-- General Public License for more details.
+-- 
+-- You should have received a copy of the GNU General Public License
+-- along with this program; if not, write to the Free Software
+-- Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+-- 02111-1307, USA.
+-- 
 
--- This is a little helper for completion interfaces.
+-- | This is a little helper for completion interfaces.
 
--- B.Zapf July 2005
-
-module Yi.Keymap.Completion 
-( CompletionTree(CT),
-  stepTree, obvious, mergeTrees, listToTree, complete)
-where
+module Yi.Keymap.Completion ( 
+        CompletionTree(CT),
+        stepTree, obvious, mergeTrees, listToTree, complete)
+   where
 
 import Data.List
 
@@ -16,7 +32,6 @@ data CompletionTree a = CT [(a,CompletionTree a)]
 
 instance (Show a) => Show (CompletionTree a) where
   show x = show' x
-
 
 show' :: (Show a) => CompletionTree a -> String
 show' (CT []) = []
