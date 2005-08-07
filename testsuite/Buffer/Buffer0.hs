1f5d3ee026131a3adc0cf46acf1e0af85de667f9
Don Stewart <dons@cse.unsw.edu.au>
2005-08-07 04:02:39 +0000

    More tests. Fix dependency tracking in testsuite makefile

--- testsuite/Buffer/Buffer0.hs
+++ testsuite/Buffer/Buffer0.hs
@@ -4,27 +4,69 @@ module Buffer.Buffer0 where
 
 import Yi.Buffer
 import Yi.FastBuffer
+import Data.Unique
 
+import Data.List
+
+import System.Directory
 import System.IO.Unsafe
+import qualified Control.Exception
+import Control.Concurrent
 
 import TestFramework
 
-contents = "contents\nstring"
+-- really test it!
+contents = unsafePerformIO $  do
+        s <- readFile "../README"
+        forkIO (Control.Exception.evaluate (length s) >> return ())
+        return s
 
 $(tests "fastBuffer" [d| 
 
- testElemsB = unsafePerformIO $ do
-             b <- newB "testbuffer" contents :: IO FBuffer
-             s <- elemsB b
-             return $ assertEqual s contents
+ testElems = unsafePerformIO $ do
+        b <- newB "testbuffer" contents :: IO FBuffer
+        s <- elemsB b
+        return $ assertEqual s contents
 
- testNameB = unsafePerformIO $ do
-             b <- newB "testbuffer" contents :: IO FBuffer
-             return $ assertEqual (nameB b) "testbuffer"
+ testName = unsafePerformIO $ do
+        b <- newB "testbuffer" contents :: IO FBuffer
+        return $ assertEqual (nameB b) "testbuffer"
 
- testSizeB = unsafePerformIO $ do
-             b <- newB "testbuffer" contents :: IO FBuffer
-             i <- sizeB b
-             return $ assertEqual i (length contents)
+ testSize = unsafePerformIO $ do
+        b <- newB "testbuffer" contents :: IO FBuffer
+        i <- sizeB b
+        return $ assertEqual i (length contents)
+
+ testWriteFile = unsafePerformIO $ do
+        b <- newB "testbuffer" contents :: IO FBuffer
+        let f = "testWriteFile.file"
+        hPutB b f
+        s <- readFile f
+        removeFile f
+        return $ assertEqual s contents
+
+ testGetFileNameB = unsafePerformIO $ do
+        b <- hNewB "../README" :: IO FBuffer
+        m <- getfileB b
+        return $ assertEqual m (Just "../README")
+
+ testSetFileNameB = unsafePerformIO $ do
+        b <- newB "testbuffer" contents :: IO FBuffer
+        setfileB b "../README"
+        m <- getfileB b
+        return $ assertEqual m (Just "../README")
+
+ testLotsOfBuffers = unsafePerformIO $ do
+        bs <- sequence [ newB (show x) contents :: IO FBuffer
+                       | x <- [1..100] ]   -- create a 1000 buffers
+        
+        bs' <- mapM elemsB bs
+
+        return $ do assertEqual (length . nub . sort $ map keyB bs) (length bs)
+                    assert $ (length . nub . sort $ bs')  == 1
 
  |])
+
+instance Show Unique where
+    showsPrec _ u = showString "<unique>"
+    
