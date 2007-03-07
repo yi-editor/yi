d88612b0e85bb224f3f655227252d6eaac6a819f
jeanphilippe.bernardy@gmail.com <unknown>
2007-03-07 07:23:03 +0000

    More sensible YiConfig.example

--- YiConfig.example.hs
+++ YiConfig.example.hs
@@ -3,9 +3,11 @@ module YiConfig ( yiMain ) where
 import Yi.Yi
 import Yi.Editor
 import Yi.Keymap.Emacs
+import Yi.Keymap.Vim
+import Control.Monad.Trans
 
 yiMain :: EditorM ()
 yiMain = do
-  modifyEditor_  $ \e ->
-      return e { defaultKeymap = Yi.Keymap.Emacs.keymap }
-  msgE "Emacs config set."
+  lift $ initDebug ".yi.dgb.user"
+  modifyEditor_  $ \e -> return e { defaultKeymap = Yi.Keymap.Vim.keymap }
+  msgE "User configuration finished."
