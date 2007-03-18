4c3d94de5a6b0d26a281705e990ea44ab6592319
jeanphilippe.bernardy@gmail.com <unknown>
2007-03-18 08:49:24 +0000

    Add example of dynamic reconfiguration

---
+++ examples/DynamicReconf.hs
@@ -0,0 +1,37 @@
+-- Don't run with --as=emacs (or any --as=) !
+-- (Otherwise you would not see your nice new keymap)
+
+module YiConfig where
+
+import Yi.Yi
+import Yi.Editor
+import Yi.Keymap.Emacs
+import Control.Monad.Trans
+-- You can import any number of packages that will be loaded at start.
+-- They will have to be used fully qualified though.
+
+yiMain :: EditorM ()
+yiMain = do
+  reconfigure
+  -- whatever code can be ran here.
+  msgE "User configuration successful."
+
+
+-- | reload YiConfig and run reconfigure.
+reconfigureE = do
+  reloadE
+  execE "YiConfig.reconfigure" -- Must be dynamic (if we want to see the new reconfigure that has (hopefully) been just loaded)
+  msgE "reconfiguration finished." 
+
+myKeymap = runKeymap (rebind [("C-h r", write $ reconfigureE),
+                              ("C-h i", write $ helpE),
+                              ("C-h t", write $ testActionE)
+                             ] normalKeymap)
+
+helpE = msgE "Change ~/.yi/YiConfig.testAction, type C-h r to reload, then C-h t to test your changes. Errors are in the *messages* buffer"
+
+testActionE = msgE "Have fun changing this! :)"
+
+
+reconfigure = do
+  changeKeymapE myKeymap
