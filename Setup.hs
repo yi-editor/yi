1fe6dfb28c5d8556dcb12ca7a05d7c24ef722cf9
JeanPhilippe.bernardy@gmail.com <unknown>
2007-11-16 22:23:39 +0000

    Copy the precompiled files in the correct place

--- Setup.hs
+++ Setup.hs
@@ -13,6 +13,7 @@ import Distribution.Simple.Setup
 import System.Directory
 import System.FilePath
 import System.IO
+import Distribution.Simple.Utils (copyFileVerbose)
 
 main :: IO ()
 main = defaultMainWithHooks defaultUserHooks
@@ -23,7 +24,6 @@ mkOpt (name,def) = "-D" ++ name ++ "=" ++ def
 
 -- TODO: add a configuration hook that does not want to build for
 -- certain combination of flags
-
 
 bHook :: PackageDescription -> LocalBuildInfo -> UserHooks -> BuildFlags -> IO ()
 bHook pd lbi hooks flags = do
@@ -72,20 +72,28 @@ precompiles = [("Yi.Main", []),
 install :: PackageDescription -> LocalBuildInfo -> UserHooks -> InstallFlags -> IO ()
 install pd lbi hooks flags = do
   curdir <- getCurrentDirectory
-  allFiles0 <- mapM unixFind $ map (curdir </>) $ ["Yi", foldl1 (</>) ["dist","build","Yi"]]
-  let allFiles = map (makeRelative curdir) $ nub $ concat allFiles0
-      sourceFiles = filter ((`elem` [".hs-boot",".hs",".hsinc"]) . takeExtension) allFiles
-      targetFiles = filter ((`elem` [".hi",".o"]) . takeExtension) allFiles
-      --NOTE: It's important that source files are copied before target files,
-      -- otherwise GHC (via Yi) thinks it has to recompile them when Yi is started.
-      pd' = pd {dataFiles = dataFiles pd ++ sourceFiles ++ targetFiles}
-  instHook defaultUserHooks pd' lbi hooks flags
+  let rel = map . makeRelative
+      buildDir = curdir </> "dist" </> "build" 
+  sourceFiles <- rel curdir   <$> unixFindExt               "Yi"  [".hs-boot",".hs",".hsinc"]
+  targetFiles <- rel buildDir <$> unixFindExt (buildDir </> "Yi") [".hi",".o"]
+  print targetFiles
+  let InstallDirs {datadir = dataPref} = absoluteInstallDirs pd lbi NoCopyDest
+      verbosity = installVerbose flags
+      copyFile :: FilePath -> FilePath -> FilePath -> IO ()
+      copyFile srcDir dstDir file = copyFileVerbose verbosity (srcDir </> file) (dstDir </> file)
+  -- NOTE: It's important that source files are copied before target files,
+  -- otherwise GHC (via Yi) thinks it has to recompile them when Yi is started.
+  mapM_ (copyFile curdir dataPref) sourceFiles
+  mapM_ (copyFile buildDir dataPref) targetFiles
+  instHook defaultUserHooks pd lbi hooks flags
+  
+unixFindExt :: FilePath -> [String] -> IO [FilePath]
+unixFindExt dir exts = filter ((`elem` exts) . takeExtension) <$> unixFind dir
 
 unixFind :: FilePath -> IO [FilePath]
 unixFind dir = do
   contents0 <- getDirectoryContents dir
   let contents = map (dir </>) $ filter (not . (`elem` [".", ".."])) contents0
-  -- putStrLn $ dir ++ " > " ++ show contents0
   dirs <- filterM doesDirectoryExist contents
   files <- filterM doesFileExist contents
   rec <- mapM unixFind dirs
