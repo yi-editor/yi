96d498eac6d6ee377ef42b2a8fab33914e708e6a
a.d.clark@ed.ac.uk <unknown>
2007-12-19 14:50:22 +0000

    Added two new templates

--- Yi/Templates.hs
+++ Yi/Templates.hs
@@ -93,6 +93,47 @@ haskellTemplates =
               , ""
               ]
     )
+  , ( "GPL-2"
+    , unlines 
+      [ "--"
+      , "-- This program is free software; you can redistribute it and/or"
+      , "-- modify it under the terms of the GNU General Public License as"
+      , "-- published by the Free Software Foundation; either version 2 of"
+      , "-- the License, or (at your option) any later version."
+      , "--"
+      , "-- This program is distributed in the hope that it will be useful,"
+      , "-- but WITHOUT ANY WARRANTY; without even the implied warranty of"
+      , "-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU"
+      , "-- General Public License for more details."
+      , "--"
+      , "-- You should have received a copy of the GNU General Public License"
+      , "-- along with this program; if not, write to the Free Software"
+      , "-- Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA"
+      , "-- 02111-1307, USA."
+      , "--"
+      ]
+    )
+  , ( "cabal-setup"
+    , unlines
+      [ "import Distribution.Simple"
+      , "import System.Cmd"
+      , "  ( system )"
+      , "import System.Exit"
+      , "  ( exitWith )"
+      , "import System"
+      , "  ( getArgs )"
+      , ""
+      , "myHooks :: UserHooks"
+      , "myHooks = defaultUserHooks"
+      , ""
+      , "main :: IO ()"
+      , "main = do args <- getArgs"
+      , "          processArgs args"
+      , ""
+      , "processArgs :: [ String ] -> IO ()"
+      , "processArgs _  = defaultMainWithHooks myHooks"
+      ]
+    )
   ]
 
 addTemplate :: String -> YiM ()
