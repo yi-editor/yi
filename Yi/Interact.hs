d15722c126c7be1005b1c2fa873c194175f14fe6
jeanphilippe.bernardy@gmail.com <unknown>
2008-01-01 21:27:39 +0000

    Interact module has nothing to do with what it was; update the license accordingly

--- Yi/Interact.hs
+++ Yi/Interact.hs
@@ -1,11 +1,24 @@
 {-# OPTIONS_GHC -fglasgow-exts -fallow-undecidable-instances #-}
 
+-- Copyright (c) Jean-Philippe Bernardy 2007-8
+--
+-- This program is free software; you can redistribute it and/or
+-- modify it under the terms of the GNU General Public License as
+-- published by the Free Software Foundation; either version 2 of
+-- the License, or (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+-- General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program; if not, write to the Free Software
+-- Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+-- 02111-1307, USA.
+
+
 {- |
-
-Copyright   :  (c) The University of Glasgow 2002, (c) Jean-Philippe Bernardy 2007
-License     :  BSD-style (see http:\/\/darcs.haskell.org\/packages\/base\/LICENSE)
-Portability :  non-portable (local universal quantification)
-
 
 This is a library of interactive processes combinators, usable to
 define extensible keymaps. 
