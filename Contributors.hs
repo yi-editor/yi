6b268bf64ccecb5051b31ebd07d644f0294c7555
JeanPhilippe.bernardy@gmail.com <unknown>
2008-01-10 19:33:48 +0000

    Update contributors script

--- Contributors.hs
+++ Contributors.hs
@@ -1,36 +1,43 @@
 {-# OPTIONS -fglasgow-exts #-}
-
+{-# LANGUAGE OverloadedStrings #-}
 import Data.Array (elems, Array)
-import Data.ByteString.Char8 (pack, unpack, ByteString) 
+import Data.ByteString.Char8 (pack, ByteString) 
 import Data.Char
-import Data.List (nub, sort, sortBy, intersperse)
+import Data.String
+import Data.List (intersperse)
 import Prelude hiding (lines, readFile)
 import Text.Regex.Posix
 import qualified Data.ByteString.Char8 as BS
 import qualified Data.Map as M
 
+instance IsString ByteString where
+    fromString = BS.pack
+
 capitalize s = BS.cons (toUpper $ BS.head s) (BS.tail s)
 
-splitN n [] = []
-splitN n l = let (c,ls) = splitAt n l in (l : splitN n ls)
-
-tx = "Thu Oct 14 05:40:06 CEST 2004 "
+tx :: String = "Thu Oct 14 05:40:06 CEST 2004 "
 
 unquote x 
     | BS.head x == '\'' && BS.last x == '\'' = unquote (BS.init $ BS.tail $ x)
     | otherwise = x
 
+type M = Array Int ByteString
+
+mkName match | [_,firstname, lastname] <- elems match
+       = BS.concat . intersperse " " . map capitalize $ [firstname, lastname]
+
 name :: ByteString -> ByteString
 name tag 
-     | match :: Array Int ByteString <- tag =~ pack "^\"?(.+)<.*>\"?$", [_,name] <- elems match = name
-     | match :: Array Int ByteString <- tag =~ pack "^<?(.*)@(.*)\\.name>?$", [_,firstname, lastname] <- elems match 
-             = BS.concat . intersperse (BS.pack " ") . map capitalize $ [firstname, lastname]
-     | match :: Array Int ByteString <- tag =~ pack "^<?(.*)@.*>?$", [_,user] <- elems match = user
-     | otherwise = tag
+     | match :: M <- tag =~ pack "^\"?(.+)<.*>\"?$", [_,name] <- elems match = name
+     | match :: M <- tag =~ pack "^<?(.*)@(.*)\\.name>?$", 
+       [_,_,_] <- elems match = mkName match 
+     | match :: M <- tag =~ pack "^<?(.*)@.*>?$", [_,user] <- elems match = nickToName user
+     | otherwise = nickToName tag
 
 trim = fst . BS.spanEnd isSpace
 
-name' x = case map toLower x of 
+nickToName :: ByteString -> ByteString
+nickToName x = case BS.map toLower x of 
             "a.d.clark"             -> "Allan Clark"
             "alson"                 -> "Alson Kemp"
             "andrii.z"              -> "Andrii Zvorygin"
@@ -38,6 +45,7 @@ name' x = case map toLower x of
             "coreyoconnor"          -> "Corey O'Connor"
             "dons"                  -> "Don Stewart"
             "grddev"                -> "Gustav Munkby"
+            "gwern0"                -> "Walter Moreira"
             "jeanphilippe.bernardy" -> "Jean-Philippe Bernardy"
             "mlang"                 -> "Mario Lang"
             "mwotton"               -> "Mark Wotton"
@@ -47,20 +55,17 @@ name' x = case map toLower x of
             "vintermann"            -> "Harald Korneliussen"
             "vivian.mcphail"        -> "Vivian McPhail"
             "zapf"                  -> "Bastiaan Zapf"
-            _                       -> x
-
-nick = pack . name' . unpack
+            _ | match :: M <- x =~ pack "^(.*)\\.(.*)$",
+                [_,_,_] <- elems match -> mkName match
+              | otherwise -> x
 
 main = do
   f <- BS.getContents
   let ls = BS.lines f
       ps = filter (not . isSpace . BS.head) $ filter (not . BS.null) $ ls
-      contrs = M.fromListWith (+) $ flip zip (repeat 1) $ map (trim . nick . name . unquote . BS.dropWhile isSpace . BS.drop (length tx)) $ ps
+      contrs = M.fromListWith (+) $ flip zip (repeat 1) $ map (trim . name . unquote . BS.dropWhile isSpace . BS.drop (length tx)) $ ps
   -- print (length ps)
   -- mapM print $ sortBy (comparing fst) $ M.toList $ contrs
   mapM BS.putStrLn $ M.keys contrs
   return ()
 
-on f g x y = f (g x) (g y)
-
-comparing f x y = compare (f x) (f y)
