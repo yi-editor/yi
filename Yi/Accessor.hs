f54d29cd272fc03ff5083805ddd9e4db6e6f2626
JeanPhilippe.bernardy@gmail.com <unknown>
2008-01-18 21:44:06 +0000

    +allA combinator

--- Yi/Accessor.hs
+++ Yi/Accessor.hs
@@ -20,7 +20,8 @@
 
 module Yi.Accessor where
 import Control.Monad.State
-
+import Data.Traversable as Traversable
+import Data.Foldable
 
 -- | A way to access and modify a part of a complex structure.
 -- Categorically, an arrow from @whole@ to @part@.
@@ -63,6 +64,16 @@ modifyA a f = modify (modifier a f)
 
 setA :: MonadState s m => Accessor s p -> p -> m ()
 setA a p = modifyA a (const p)
+
+
+allA :: forall whole part t. Traversable t => Accessor whole part -> Accessor (t whole) (t part)
+allA (Accessor g m) = Accessor (fmap g) modifier'
+    where modifier' mapParts wholes = distribute wholes (toList $ mapParts $ fmap g wholes)
+          distribute wholes parts = fst $ runState (Traversable.mapM setOne wholes) parts
+          setOne whole = do
+            h' <- gets head
+            modify tail
+            return (m (const h') whole)
 
 -- (#=) :: MonadState s m => Accessor s p -> p -> m ()
 -- (#=) = setA
