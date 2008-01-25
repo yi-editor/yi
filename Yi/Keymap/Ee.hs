e89e11b00096a8ce08710fa12132ad4ae177a70b
gwern0@gmail.com <unknown>
2008-01-25 19:37:47 +0000

    Keymap/Ee.hs: partial update of movement commands; file is still broken

--- Yi/Keymap/Ee.hs
+++ Yi/Keymap/Ee.hs
@@ -1,20 +1,16 @@
---
--- Copyright (c) 2005 Don Stewart - http://www.cse.unsw.edu.au/~dons
---
---
+-- Copyright (c) 2005, 2008 Don Stewart - http://www.cse.unsw.edu.au/~dons
 
---
 -- Introductory binding for the 'ee' editor
---
 
 module Yi.Keymap.Ee ( keymap ) where
 
 import Yi.Yi
+import Control.Arrow
+import Yi.Keymap.Emacs.KillRing
 
 keymap :: Keymap
 keymap = comap eventToChar (command +++ insert)
 
---
 -- Control keys:
 -- ^a ascii code           ^i tab                  ^r right
 -- ^b bottom of text       ^j newline              ^t top of text
@@ -25,24 +21,23 @@ keymap = comap eventToChar (command +++ insert)
 -- ^g begin of line        ^o end of line          ^y delete line
 -- ^h backspace            ^p prev page            ^z undelete line
 -- ^[ (escape) menu
---
 
 insert :: Interact Char ()
-insert  = do c <- satisfy (const True); write (insertE c)
+insert  = do c <- satisfy (const True); write (insertN c)
 
-command :: Interact Char () 
+command :: Interact Char ()
 command = choice [event c >> write act | (c, act) <- cmds]
-    where cmds = [('\^R', rightE          ),  
-                  ('\^B', botE            ),
-                  ('\^T', topE            ),
-                  ('\^K', deleteE         ),
-                  ('\^U', upE             ),
-                  ('\^D', downE           ),
-                  ('\^L', leftE           ),
-                  ('\^G', solE            ),
-                  ('\^O', eolE            ),
-                  ('\^Y', killE           ),
-                  ('\^H', deleteE >> leftE),
+    where cmds = [('\^R', rightB          ),
+                  ('\^B', botB            ),
+                  ('\^T', topB            ),
+                  ('\^K', deleteN 1       ),
+                  ('\^U', execB Move VLine Backward),
+                  ('\^D', execB Move VLine Forward),
+                  ('\^L', leftB           ),
+                  ('\^G', moveToSol       ),
+                  ('\^O', moveToEol       ),
+                  ('\^Y', killLineE       ),
+                  ('\^H', bdeleteB        ),
                   ('\^X', quitE           )]
 
 
