9d7ba69b660df98b5392e4f0715abe125f70092e
JeanPhilippe.bernardy@gmail.com <unknown>
2008-02-16 12:32:13 +0000

    Wall police

--- Yi.hs
+++ Yi.hs
@@ -14,14 +14,20 @@ main0 cfg = do
   initDebug ".yi.dbg"
   Yi.main cfg
 
+recoverState :: String -> IO ()
 recoverState _stateName = return ()
 
+realMain :: Config -> yiState -> IO ()
 realMain cfg _state = main0 cfg
 
+projectName :: String
 projectName = "yi"
 
+initState :: () -- TODO: Should be Editor
 initState = ()
 
+driver :: IO ()
 driver = mainMaster projectName initState (realMain defaultConfig)
 
+yi :: Config -> IO ()
 yi cfg = mainSlave recoverState () (realMain cfg)
