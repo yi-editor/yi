49c595194d2cab3a8b013426be3659e5f956c700
a.d.clark@ed.ac.uk <unknown>
2008-02-21 12:13:37 +0000

    Cleanup of Yi/Indent and haskell keyword indentation hints
    You can now specify a list of keywords to use as hints for indentation.
    Keywords are specified in two lists where by the first list uses a keyword
    on the previous line as an indentation hint itself.
    So for example if specify 'then' and 'else' and I have the line:
    
    if blah then blah2 else blah3
            ^          ^
    
    then indentation hints will be given where the carots are.
    The second list uses keywords by returning as a hint the first non-white
    space character after the keyword, so if 'where' is in this list then
    
    my_expression where x = 1
                        ^
    will be a keyword.
    
    Both styles of hints work with tab characters and the indentation hints even within the line.
    
    Note though that this will *not* be on by default (since Haskell indentation is questionable for
    other kinds of files).
    
    To enable this: you want to add something to your ~/.yi/yi.hs (or HackerMain.hs) file like:
    
    
    main :: IO ()
    main = yi $ defaultConfig { startFrontEnd    = Vty.start
                              , defaultKm        = myKeyMap
                              , publishedActions = myAndDefaultActions
                              }
    
    
    -- So your keymap will be the emacs keybindings
    -- 'keymap' with your additional/overriding key
    -- binding definitions.
    myKeyMap :: KeymapM ()
    myKeyMap = rebind myKeys keymap
    
    -- The list of keys that you wish to override/add to
    -- the default emacs keybindings.
    -- Add/remove from here as you desire.
    myKeys :: KList
    myKeys = 
      [ -- Use the tab completion with haskell keyword hints
        -- since I'm mostly editing haskell code.
        ("C-i",      write autoIndentHaskellB )
    
    *NOTE: I had to use "C-i" as "TAB" didn't work here*
    If you disagree with my sets of keywords (defined in Yi/Indent.hs) you can make your own
    by calling 'autoIndentWithKeywordsB'.
    In fact I have factored out the logic to get the previous indentations and hints from
    the previous line so you can configure this quite a bit by calling:
    'autoIndentHelperB'
    
    I believe this fixes issue number: 72.

--- Yi/Indent.hs
+++ Yi/Indent.hs
@@ -30,18 +30,18 @@ data IndentSettings = IndentSettings {  expandTabs :: Bool
                                      }
                       deriving (Eq, Show, Typeable)
 
-{- The default indent settings should likely be initializable from a global
- - preference.
- - Currently the initial settings reflect what's usually used in Yi's sources.
- - One aspect that is common in Yi that is not covered is that the first
- - indent is usually 4 spaces instead of 2.
+{- 
+  The default indent settings should likely be initializable
+  from a global preference.
  -}
 instance Initializable IndentSettings where
     initial = IndentSettings True 2 2
 
-{- Inserts either a \t or the number of spaces specified by tabSize in the
- - IndentSettings
- -}
+{- |
+  Inserts either a \t or the number of spaces specified by tabSize in the
+  IndentSettings. Note that if you actually want to insert a tab character
+  (for example when editing makefiles) then you should use: @insertB '\t'@.
+-}
 insertTabB :: BufferM ()
 insertTabB = do
   indentSettings <- indentSettingsB
@@ -49,68 +49,199 @@ insertTabB = do
     then replicate (tabSize indentSettings) ' '
     else ['\t']
 
+{-|
+  Retrieve the current indentation settings.
+-}
 indentSettingsB :: BufferM IndentSettings
 indentSettingsB = getDynamicB
 
 
-fetchPreviousIndentsB :: BufferM [Int]
-fetchPreviousIndentsB = do
-  p0 <- pointB
-  lineUp
-  p1 <- pointB
-  l <- readLnB
-  i <- indentOfB l
-  if p0 == p1 || i == 0 then return [0] else do
-    is <- fetchPreviousIndentsB
-    return (i:is)
-
-cycleIndentsB :: [Int] -> BufferM ()
-cycleIndentsB indents = do
-  l <- readLnB
-  curIndent <- indentOfB l
-  let (below, above) = span (< curIndent) $ indents
-  -- msgEditor $ show (below, above)
-  indentToB $ last (above ++ below)
-
+{-|
+  A specialisation of 'autoIndentHelperB'.
+  This is the most basic and the user is encouraged to
+  specialise 'autoIndentHelperB' on their own.
+-}
 autoIndentB :: BufferM ()
-autoIndentB = do
-  is  <- savingExcursionB fetchPreviousIndentsB
-  pl  <- getPreviousLineB
-  pli <- indentOfB pl
-  let i       = lastOpenBracket pl
-      indents = pli+2 : i : is
-  cycleIndentsB $ sort $ nub indents
+autoIndentB =
+  autoIndentHelperB fetchPreviousIndentsB indentsOfString
+  where
+  -- Returns the indentation hints considering the given
+  -- string as the line above the current one.
+  -- The hints added are:
+  --     The indent of the given string
+  --     The indent of the given string plus two
+  --     The offset of the last open bracket if any in the line.
+  indentsOfString :: String -> BufferM [ Int ]
+  indentsOfString input = 
+    do indent       <- indentOfB input
+       bracketHints <- lastOpenBracketHint input
+       return $ indent : (indent + 2) : bracketHints
+
+{-|
+  This takes two arguments the first is a function to
+  obtain indentation hints from lines above the current one.
+  The second is a function to obtain a set of indentation hints
+  from the previous line. Both of these are in the 'BufferM'
+  monad although the second seems like it is unnecessary.
+  However we must take into account the length of tabs which come
+  from the the tab settings and hence we must be in the 'BufferM'
+  monad.
+
+  To get the straightforward behaviour of the indents of all previous
+  lines until one of them has zero indent call this with:
+  @autoIndentHelperB fetchPreviousIndentsB (liftM (: []) indentOfB)@
+  However commonly we wish to have something more interesting for
+  the second argument, in particular we commonly wish to have the
+  last opening bracket of the previous line as well as its indent.
+-}
+autoIndentHelperB :: -- | Action to fetch hints from previous lines
+                     BufferM [ Int ] 
+                     -- | Action to calculate hints from previous line
+                 -> (String -> BufferM [ Int ])
+                     -- | Returned action to automatically indent.
+                 -> BufferM ()
+autoIndentHelperB getUpwards getPrevious = 
+  do upwardHints   <- savingExcursionB getUpwards
+     previousLine  <- getPreviousLineB
+     previousHints <- getPrevious previousLine
+     let allHints = sort $ nub (upwardHints ++ previousHints)
+     cycleIndentsB allHints
+  where
+  -- Cycles through the indentation hints. It does this without
+  -- requiring to set/get any state. We just look at the current
+  -- indentation of the current line and moving to the largest
+  -- indent that is 
+  cycleIndentsB :: [ Int ] -> BufferM ()
+  cycleIndentsB []      = return ()
+  cycleIndentsB indents =
+    do currentLine   <- readLnB
+       currentIndent <- indentOfB currentLine
+       let (below, above) = span (< currentIndent) indents
+       -- msgEditor $ show (below, above)
+       -- So in particular if 'below' is null then we will
+       -- go to the largest indentation, if below is not null
+       -- we go to the largest indentation which is *not* higher
+       -- than the current one.
+       indentToB $ last (above ++ below)
+
+{-|
+  A function generally useful as the first argument to
+  'autoIndentHelperB'. This searches the lines above
+  the current line for the indentations of each line
+  until we get to a line which has no indentation
+  *and* is not empty. Indicating that we have reached
+  the outer scope.
+-}
+fetchPreviousIndentsB :: BufferM [Int]
+fetchPreviousIndentsB = 
+     -- Move up one line, 
+  do moveOffset <- lineMoveRel (-1)
+     line       <- readLnB
+     indent     <- indentOfB line
+     -- So if we didn't manage to move upwards
+     -- or the current offset was zero *and* the line
+     -- was non-blank then we return just the current
+     -- indent (it might be the first line but indented some.)
+     if moveOffset == 0 ||
+        ( indent == 0 &&
+          (any (not . isSpace) line) )
+        then return [ indent ]
+        else liftM (indent :) fetchPreviousIndentsB
 
 
--- Returns the position of the last opening bracket on the
+{-| An application of 'autoIndentHelperB' which adds more
+    indentation hints using the given keywords.
+    The offsets of the first set of keywords are used as hints.
+    For the second set of keywords it is not the offsets of the
+    keywords themselves but the offset of the first non-white
+    characters after the keywords.
+
+    In addition to the keyword hints we also do the same as the
+    default ('autoIndentB') which is to use any non-closed
+    opening brackets as hints.
+-}
+autoIndentWithKeywordsB :: [ String ] -> [ String ] -> BufferM ()
+autoIndentWithKeywordsB firstKeywords secondKeywords =
+  autoIndentHelperB fetchPreviousIndentsB getPreviousLineHints
+  where
+  getPreviousLineHints :: String -> BufferM [ Int ]
+  getPreviousLineHints input =
+    do indent       <- indentOfB input
+       bracketHints <- lastOpenBracketHint input
+       keyHintsOne  <- keywordHints firstKeywords input
+       keyHintsTwo  <- keywordAfterHints secondKeywords input
+       return $ indent : (indent + 2) : ( bracketHints ++
+                                          keyHintsOne  ++
+                                          keyHintsTwo )
+
+
+{-| For haskell these are reasonable settings but you might be able
+    to come up with something better in your own ~/yi.hs file.
+-}
+autoIndentHaskellB :: BufferM ()
+autoIndentHaskellB =
+  autoIndentWithKeywordsB [ "if"
+                          , "then"
+                          , "else"
+                          , "|"
+                          , "->"
+                          , "case" -- hmm
+                          , "in"
+                          -- Note tempted by having '=' in here that would
+                          -- potentially work well for 'data' declarations
+                          -- but I think '=' is so common in other places
+                          -- that it would introduce many spurious/annoying
+                          -- hints.
+                          ]
+                          [ "where"
+                          , "let"
+                          , "do"
+                          , "mdo"
+                          , "{-"
+                          , "--"
+                          ]
+
+
+
+-- | Returns the position of the last opening bracket on the
 -- line which is not closed on the same line.
 -- Note that if we have unmatched parentheses such as "( ]"
 -- then we may not get the correct answer, but in that case
 -- then arguably we don't really care if we get the correct
 -- answer (at least if we get it wrong the user may notice
 -- their error).
-lastOpenBracket :: String -> Int
-lastOpenBracket s =
-  getOpen 0 $ reverse s
+-- We return a list here as it's a convenient way of returning
+-- no hint in the case of there being no non-closed bracket
+-- and normally such a hint will be part of a list of hints
+-- anyway.
+-- NOTE: this could be easily modified to return the indentations
+-- of *all* the non-closed opening brackets. But I think this is
+-- not what you generally want.
+-- TODO: we also do not care whether or not the bracket is within
+-- a string or escaped. If someone feels up to caring about that
+-- by all means please fix this.
+lastOpenBracketHint :: String -> BufferM [ Int ]
+lastOpenBracketHint input =
+  case getOpen 0 $ reverse input of
+    Nothing -> return []
+    Just s  -> liftM (: []) $ spacingOfB s
   where
   -- We get the last open bracket by counting through
   -- the reversed line, when we see a closed bracket we
   -- add one to the count. When we see an opening bracket
-  -- decrease the count. If we see a close bracket when the
-  -- count is 0 we return the remaining length of the
-  -- (reversed) string as the indentation.
-  -- Note then that this assumes that the parentheses are
-  -- well matched, although it arguably does the correct thing
-  -- in the case that they are not.
-  getOpen :: Int -> String -> Int
-  -- If we don't see an unmatched open bracket then we just
-  -- have to return 0.
-  getOpen _ [] = 0
+  -- decrease the count. If we see an opening bracket when the
+  -- count is 0 we return the remaining (reversed) string
+  -- as the part of the line which preceds the last opening bracket.
+  -- This can then be turned into an indentation by calling 'spacingOfB'
+  -- on it so that tabs are counted as tab length.
+  -- NOTE: that this will work even if tab occur in the middle of the line
+  getOpen :: Int -> String -> Maybe String
+  -- We of course return nothing, there is no bracket to give a hint.
+  getOpen _ []               = Nothing
   getOpen i (c : rest)
       -- If it is opening and we have no closing to match
-      -- then we return the length of the rest of the line
-      -- as the indentation.
-    | isOpening c && i == 0 = length rest
+      -- then we return the rest of the line
+    | isOpening c && i == 0 = Just rest
       -- If i is not zero then we have matched one of the
       -- closing parentheses and we can decrease the nesting count.
     | isOpening c           = getOpen (i - 1) rest
@@ -132,36 +263,139 @@ lastOpenBracket s =
   isClosing '}' = True
   isClosing _   = False
 
+
+-- | Returns the offsets of all the given keywords
+-- within the given string. This is potentially useful
+-- as providing indentation hints.
+keywordHints :: [ String ] -> String -> BufferM [ Int ]
+keywordHints keywords =
+  getHints 0
+  where
+  -- Calculate the indentation hints of keywords from the
+  -- given string. The first argument is the current offset.
+  -- NOTE: that we have to take into account how long tab characters
+  -- are according to the indentation settings.
+  getHints :: Int -> String -> BufferM [ Int ]
+  getHints _i [] = return []
+  getHints i input@(_ : inputTail)
+    -- If there are no non-white characters left return zero hints.
+    | null rest                              = return []
+    -- Check if there are white space characters at the front and if
+    -- so then calculate the ident of it and carry on.
+    | not $ null white                       = do spaceSize <- spacingOfB white
+                                                  getHints (i + spaceSize) rest
+    -- If there are no white space characters check if we are looking
+    -- at a keyword and if so add it as a hint
+    | any ((flip isPrefixOf) input) keywords = liftM (i :) tailHints
+    -- Finally we just continue with the tail.
+    | otherwise                              = tailHints
+    where
+    (white, rest) = span isSpace input
+    tailHints     = getHints (i + 1) inputTail
+
+-- | Returns the offsets of anything that isn't white space 'after'
+-- a keyword on the given line.
+-- This is essentially then the same as 'keywordHints' except that
+-- for each keyword on the input rather than return the offset at
+-- the start of the keyword we return the offset of the first non-white
+-- character after the keyword.
+keywordAfterHints :: [ String ] -> String -> BufferM [ Int ]
+keywordAfterHints keywords =
+  getHints 0
+  where
+  -- Calculate the indentation hints of keywords from the
+  -- given string. The first argument is the current offset.
+  -- NOTE: that we have to take into account how long tab characters
+  -- are according to the indentation settings.
+  getHints :: Int -> String -> BufferM [ Int ]
+  getHints _i [] = return []
+  getHints i input@(_ : inputTail)
+    -- If there is any preceding white space then just take the length
+    -- of it (according to the indentation settings and proceed.
+    | not $ null indentation      = do indent <- spacingOfB indentation
+                                       getHints (i + indent) nonWhite
+    -- If there is a keyword at the current position and
+    -- the keyword isn't the last thing on the line.
+    | any ((flip isPrefixOf) input) keywords
+      && (not $ null afterwhite)  =  do indent    <- spacingOfB white
+                                        let hint  =  i + (length key) + indent
+                                        tailHints <- getHints hint afterwhite
+                                        return $ hint : tailHints
+    -- we don't have a hint and we can re-try for the rest of the line
+    | otherwise                   = getHints (i + 1) inputTail
+    where
+    -- Split the input into the preceding white space and the rest
+    (indentation, nonWhite) = span isSpace input
+
+    -- The keyword and what is after the keyword
+    -- this is only used if 'indentation' is null so we needn't worry that
+    -- we are taking from the input rather than 'nonWhite'
+    (key, afterkey)     = break isSpace input
+    -- The white space and what is after the white space
+    (white, afterwhite) = span  isSpace afterkey
+
+
+{-|
+  Returns the indentation of a given string. Note that this depends
+  on the current indentation settings.
+-}
 indentOfB :: String -> BufferM Int
 indentOfB = spacingOfB . takeWhile isSpace
 
+{-| Returns the length of a given string taking into account the
+    white space and the indentation settings.
+-}
 spacingOfB :: String -> BufferM Int
-spacingOfB text = do
-  indentSettings <- indentSettingsB
-  let spacingOfChar '\t' = tabSize indentSettings
-      spacingOfChar _ = 1
-  return $ sum $ map spacingOfChar text
+spacingOfB text = 
+  do indentSettings <- indentSettingsB
+     let spacingOfChar :: Char -> Int
+         spacingOfChar '\t' = tabSize indentSettings
+         spacingOfChar _    = 1
+     return (sum $ map spacingOfChar text)
 
+{-| Indents the current line to the given indentation level.
+    In addition moves the point according to where it was on the
+    line originally. If we were somewhere within the indentation
+    (ie at the start of the line or on an empty line) then we want
+    to just go to the end of the (new) indentation.
+    However if we are currently pointing somewhere within the text
+    of the line then we wish to remain pointing to the same character.
+-}
 indentToB :: Int -> BufferM ()
-indentToB level = do
-  l   <- readLnB
-  cur <- offsetFromSol
-  moveToSol
-  deleteToEol
-  let (curIndent, restOfLine) = span isSpace l
-      origOffsetFromIndent = cur - (length curIndent)
-      newFromEol = if origOffsetFromIndent <= 0
+indentToB level = 
+     -- Grab the current line
+  do line          <- readLnB
+     -- grab the current offset (in characters) from the start of the line
+     currentOffset <- offsetFromSol
+     -- move to the start of the line
+     moveToSol
+     -- delete the whole of the line.
+     deleteToEol
+         -- Separate the white space of the current line from the rest of it.
+     let (curIndent, restOfLine) = span isSpace line
+         -- The original offset (in characters) from the start of the rest
+         -- the line, this will be greater than zero if the current point
+         -- was somewhere after the indentation of the current line.
+         origOffsetFromIndent    = currentOffset - (length curIndent)
+         -- This calculates how far we should be from the end of the line.
+         -- if we are within the line then we wish to remain at the same
+         -- character, however if we were anywhere within the indentation
+         -- we wish to go to the start of the 'non' indentation, that is
+         -- to the end of the (new) indentation.
+         newFromEol = if origOffsetFromIndent <= 0
                       then length restOfLine
                       else (length restOfLine - origOffsetFromIndent)
-  insertN (replicate level ' ' ++ restOfLine)
-  leftN newFromEol
+     -- Insert the new line
+     insertN (replicate level ' ' ++ restOfLine)
+     -- Then move to the place we have calculated
+     leftN newFromEol
 
 -- | Indent as much as the previous line
 indentAsPreviousB :: BufferM ()
 indentAsPreviousB =
-  do pl  <- getPreviousNonBlankLineB
-     pli <- indentOfB pl
-     indentToB pli
+  do previousLine   <- getPreviousNonBlankLineB
+     previousIndent <- indentOfB previousLine
+     indentToB previousIndent
 
 
 -- | shiftIndent num
