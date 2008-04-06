130e491541b870d1bfafc93c26a924ed0b9b3466
JeanPhilippe.bernardy@gmail.com <unknown>
2008-04-06 12:42:42 +0000

    change errorStyle so it's visible in gtk frontend as well

--- Yi/Style.hs
+++ Yi/Style.hs
@@ -93,7 +93,7 @@ numberStyle :: Style
 numberStyle = darkredA
 
 errorStyle :: Style -> Style
-errorStyle (Style fg _) = Style fg red
+errorStyle (Style fg bg) = Style red bg
 
 
 blackA , greyA, darkredA, redA, darkgreenA, greenA, brownA :: Style
