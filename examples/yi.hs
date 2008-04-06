91b0ba363281629e88453c52cf388fd01ada222d
JeanPhilippe.bernardy@gmail.com <unknown>
2008-04-06 12:39:16 +0000

    modify yi.hs to show how to access clever haskell mode

--- examples/yi.hs
+++ examples/yi.hs
@@ -2,10 +2,26 @@ import Yi
 import Yi.Keymap.Emacs (keymap)
 import qualified Yi.Mode.Shim as Shim
 import Yi.UI.Common (UIConfig(..))
+import Yi.Modes
+import Data.List (isSuffixOf)
+import Yi.Prelude
+import Prelude ()
+
+bestHaskellMode :: Mode
+bestHaskellMode = cleverHaskellMode 
+ {
+  modeKeymap = modeKeymap Shim.mode
+ }
+
+myModetable :: ReaderT String Maybe Mode
+myModetable = ReaderT $ \fname -> case () of 
+                        _ | ".hs" `isSuffixOf` fname -> Just bestHaskellMode
+                        _ ->  Nothing
+
 
 main :: IO ()
 main = yi $ defaultConfig {
+                           modeTable = myModetable <|> modeTable defaultConfig,
                            configUI = UIConfig { configFontSize = Just 10 },
-                           defaultKm = keymap,
-                           modeTable = Shim.modeTable <|> modeTable defaultConfig
+                           defaultKm = keymap
                           }
