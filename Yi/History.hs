f5f5a6f12bfc08e6abc4196693da72f15a8c7f29
jeanphilippe.bernardy@gmail.com <unknown>
2008-04-24 07:47:30 +0000

    generalize history infrastructure

--- Yi/History.hs
+++ Yi/History.hs
@@ -13,6 +13,12 @@ import Data.Dynamic
 import Yi.Dynamic
 import Yi.Editor
 import Yi.Accessor
+import qualified Data.Map as M
+
+type Histories = M.Map String History
+
+instance (Typeable k, Typeable v) => Initializable (M.Map k v) where
+    initial = M.empty
 
 data History = History {_historyCurrent :: Int,
                         _historyContents :: [String]}
@@ -21,6 +27,13 @@ data History = History {_historyCurrent :: Int,
 instance Initializable History where
     initial = (History (-1) [])
 
+dynKeyA :: (Initializable v, Ord k) => k -> Accessor (M.Map k v) v
+dynKeyA k = Accessor (maybe initial id . M.lookup k) (\f -> M.alter (upd f) k)
+    where upd f Nothing = Just (f initial)
+          upd f (Just x) = Just (f x)
+
+
+miniBuffer = "minibuffer"
 
 historyUp :: EditorM ()
 historyUp = historyMove 1
@@ -29,37 +42,50 @@ historyDown :: EditorM ()
 historyDown = historyMove (-1)
 
 historyStart :: EditorM ()
-historyStart = do
-  (History _cur cont) <- getA dynA
-  setA dynA (History 0 (nub ("":cont)))
+historyStart = historyStartGen miniBuffer
+
+historyStartGen ident = do
+  (History _cur cont) <- getA (dynKeyA ident .> dynA)
+  setA (dynKeyA ident .> dynA) (History 0 (nub ("":cont)))
   debugHist
 
 historyFinish :: EditorM ()
-historyFinish = do
-  (History _cur cont) <- getA dynA
+historyFinish = historyFinishGen miniBuffer
+
+historyFinishGen ident = do
+  (History _cur cont) <- getA (dynKeyA ident .> dynA)
   curValue <- withBuffer0 elemsB
-  setA dynA $ History (-1) (nub $ dropWhile null $ (curValue:cont))
+  setA (dynKeyA ident .> dynA) $ History (-1) (nub $ dropWhile null $ (curValue:cont))
 
 -- TODO: scrap
 debugHist :: EditorM ()
 debugHist = return ()
+  
 
 historyMove :: Int -> EditorM ()
-historyMove delta = do
-  (History cur cont) <- getA dynA
-  curValue <- withBuffer0 elemsB
+historyMove delta = replaceBufferContent =<< historyMoveGen miniBuffer delta (withBuffer0 elemsB) 
+
+replaceBufferContent :: String -> EditorM ()
+replaceBufferContent newvalue = withBuffer0 $ do
+              sz <- sizeB
+              moveTo 0
+              deleteN sz
+              insertN newvalue
+
+
+historyMoveGen :: String -> Int -> EditorM String -> EditorM String
+historyMoveGen ident delta getCurValue = do
+  (History cur cont) <- getA (dynKeyA ident .> dynA) 
+  curValue <- getCurValue
   let len = length cont
       next = cur + delta
       nextValue = cont !! next
   case (next < 0, next >= len) of
-    (True, _) -> printMsg "end of history, no next item."
-    (_, True) -> printMsg "beginning of history, no previous item."
+    (True, _) -> do printMsg $ "end of " ++ ident ++ " history, no next item." 
+                    return curValue
+    (_, True) -> do printMsg $ "beginning of " ++ ident ++ " history, no previous item."
+                    return curValue
     (_,_) -> do
-         setA dynA (History next (take cur cont ++ [curValue] ++ drop (cur+1) cont))
+         setA (dynKeyA ident .> dynA) (History next (take cur cont ++ [curValue] ++ drop (cur+1) cont))
          debugHist
-         withBuffer0 $ do
-              sz <- sizeB
-              moveTo 0
-              deleteN sz
-              insertN nextValue
-
+         return nextValue
