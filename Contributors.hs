61421c56303f2cd56d768377b2cdb41cddc96772
jeanphilippe.bernardy@gmail.com <unknown>
2008-07-22 14:48:42 +0000

    update Contributors

--- Contributors.hs
+++ Contributors.hs
@@ -2,17 +2,21 @@
 import Data.Array (elems, Array)
 import Data.ByteString.Char8 (pack, ByteString)
 import Data.Char
+import Data.Function
 import Data.String
-import Data.List (intersperse)
+import Data.List 
 import Prelude hiding (lines, readFile)
-import Text.Regex.Posix
-import Text.Regex.Posix.ByteString
+import Text.Regex.Base
+import Text.Regex.TDFA
 import qualified Data.ByteString.Char8 as BS
 import qualified Data.Map as M
 
 capitalize s = BS.cons (toUpper $ BS.head s) (BS.tail s)
 
 tx :: String = "Thu Oct 14 05:40:06 CEST 2004 "
+
+instance IsString ByteString where
+    fromString = pack
 
 unquote x
     | BS.head x == '\'' && BS.last x == '\'' = unquote (BS.init $ BS.tail $ x)
@@ -21,8 +25,8 @@ unquote x
 mkName firstname lastname = BS.concat . intersperse " " . map capitalize $ [firstname, lastname]
 
 name :: ByteString -> ByteString
-name "andy@nobugs.org" = "Andrew Birkett"
 name tag
+     | tag == "andy@nobugs.org" = "Andrew Birkett"
      | AllTextSubmatches [_,name] <- tag =~ pack "^\"?(.+)<.*>\"?$" = name
      | AllTextSubmatches [_,firstname,lastname] <- tag =~ pack "^<?(.*)@(.*)\\.name>?$"
                                                 = mkName firstname lastname
@@ -64,8 +68,8 @@ main = do
   let ls = BS.lines f
       ps = filter (not . isSpace . BS.head) $ filter (not . BS.null) $ ls
       contrs = M.fromListWith (+) $ flip zip (repeat 1) $ map (trim . name . unquote . BS.dropWhile isSpace . BS.drop (length tx)) $ ps
-  -- print (length ps)
-  -- mapM print $ sortBy (comparing fst) $ M.toList $ contrs
+  print (length ps)
+  mapM print $ sortBy (compare `on` snd) $ M.toList $ contrs
   mapM BS.putStrLn $ M.keys contrs
   return ()
 
