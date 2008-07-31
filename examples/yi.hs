06af28555ce8d03f2bb6edad6e55fd7ec3a1746b
jeanphilippe.bernardy@gmail.com <unknown>
2008-07-31 12:22:46 +0000

    More stuff in example yi.hs   * unicode   * increase and decrease indent

--- examples/yi.hs
+++ examples/yi.hs
@@ -6,10 +6,17 @@ import Yi.Keymap.Emacs (keymap)
 -- If configured with ghcAPI, Shim Mode can be enabled:
 -- import qualified Yi.Mode.Shim as Shim
 import Yi.Mode.Haskell
-import Data.List (isSuffixOf)
+import Data.List (isSuffixOf, drop)
 import Yi.Prelude
 import Prelude ()
 import Yi.Keymap.Keys
+import Yi.String
+
+increaseIndent :: BufferM ()
+increaseIndent = modifyExtendedSelectionB Line $ mapLines (' ':)
+
+decreaseIndent :: BufferM ()
+decreaseIndent = modifyExtendedSelectionB Line $ mapLines (drop 1)
 
 myModetable :: ReaderT String Maybe AnyMode
 myModetable = ReaderT $ \fname -> case () of 
@@ -23,15 +30,89 @@ myModetable = ReaderT $ \fname -> case () of
                               -- uncomment this for Shim (dot is important!)
                               -- . modeKeymap Shim.mode
                             }
-
+greek :: [(String, String)]
 greek = [("alpha", "α"),
          ("beta", "β"),
          ("gamma", "γ"),
          ("delta", "δ")
         ]
 
+symbols :: [(String, String)]
+symbols = 
+ [
+ -- parens
+  ("<","⟨")
+ ,(">","⟩")
+ ,("[[","⟦")
+ ,("]]","⟧")
+
+ -- operators
+ ,("<|","◃") 	   
+ ,("|>","▹")
+ ,("v","∨")
+ ,("u","∪")
+ ,("V","⋁")
+ ,("^","∧")
+ ,("o","∘")
+ ,(".","·")
+ ,("x","×")
+
+ --- arrows
+ ,("<-","←")
+ ,("->","→")
+ ,("|->","↦")
+ ,("<-|","↤")
+ ,("<--","⟵")
+ ,("-->","⟶")
+ ,("|-->","⟼")
+ ,("==>","⟹")
+ ,("=>","⇒")
+ ,("<=","⇐")
+
+ --- relations
+ ,("c=","⊆") 
+ ,("c","⊂")    
+
+ ---- equal signs
+ ,("=def","≝")
+ ,("=?","≟")
+ ,("=-","≡")
+ ,("~=","≃")
+ ,("/=","≠")
+
+ -- misc
+ ,("_|_","⊥")
+ ,("T","⊤")
+ ,("|N","ℕ")
+ ,("|P","ℙ")
+ ,("^n","ⁿ")
+ ,("::","∷")
+
+ -- dashes
+ ,("-","−")
+ ]
+
+
+-- Alternatives
+--         ,("<|","◁")
+
+
+-- More:
+-- arrows: ↢ ↣ ↝ ↜  ↔ ⇤ ⇥ ⇸ ⇆
+
+-- set: ∅ ∉ ∈ ⊇ ⊃
+-- relations: ≝ ≤ ≥
+-- circled operators: ⊕⊖⊗⊘ ⊙⊚⊛⊜⊝⍟ ⎊⎉
+-- squared operators: ⊞⊟⊠⊡ 
+-- turnstyles: ⊢⊣⊤⊥⊦⊧⊨⊩⊬⊭
+-- parens: ⟪ ⟫
+
+
+
 extraInput :: Keymap
-extraInput = choice [pString ('\\':i) >>! insertN o | (i,o) <- greek]
+extraInput 
+    = choice [pString ('\\':i) >>! insertN o | (i,o) <- greek] <|>
+      choice [pString ('`':i) >>! insertN o | (i,o) <- symbols] 
 
 main :: IO ()
 main = yi $ defaultConfig {
@@ -39,4 +120,6 @@ main = yi $ defaultConfig {
                            modeTable = myModetable <|> modeTable defaultConfig,
                            configUI = (configUI defaultConfig) { configFontSize = Just 10 },
                            defaultKm = extraInput <|| keymap
+                              <|> (ctrl (char '>') ?>>! increaseIndent)
+                              <|> (ctrl (char '<') ?>>! decreaseIndent)
                           }
