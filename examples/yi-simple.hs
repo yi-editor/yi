834375cafa42f9d674e10dcc9685c43c75836137
gwern0@gmail.com <unknown>
2008-08-02 14:08:54 +0000

    fix breakage in .examples/yi-simple.hs modifyLines got renamed, but no one updated this.

--- examples/yi-simple.hs
+++ examples/yi-simple.hs
@@ -1,18 +1,18 @@
 import Yi
 import Yi.Keymap.Emacs as Emacs
-import Yi.String (modifyLines)
+import Yi.String (mapLines)
 
 increaseIndent :: BufferM ()
 increaseIndent = do
-  r <- getSelectRegionB 
+  r <- getSelectRegionB
   r' <- unitWiseRegion Line r -- extend the region to full lines.
-  modifyRegionB (modifyLines (' ':)) r'
-                                     
+  modifyRegionB (mapLines (' ':)) r'
+
 
 main :: IO ()
-main = yi $ defaultConfig 
+main = yi $ defaultConfig
        {
-         defaultKm = Emacs.keymap <|> 
+         defaultKm = Emacs.keymap <|>
                        (ctrl (char '>') ?>>! increaseIndent)
          -- bind the function to Ctrl-> and mix with default Emacs keymap.
        }
