a11988eb5db2788c329447b20325d9c25e550648
jeanphilippe.bernardy@gmail.com <unknown>
2008-08-21 09:12:03 +0000

    improve documentation of examples/yi.hs

--- examples/yi.hs
+++ examples/yi.hs
@@ -9,33 +9,45 @@ import Yi.Keymap.Emacs (keymap)
 import Yi.Keymap.Cua (keymap)
 import Yi.Keymap.Vim (keymap)
 
--- Import the desired UI
+-- Import the desired UI as needed. 
+-- Some are not complied in, so we import none here.
+
 -- import Yi.UI.Vty (start)
 -- import Yi.UI.Gtk (start)
 -- import Yi.UI.Cocoa (start)
 -- import Yi.UI.Pango (start)
 
 
+defaultUIConfig = configUI defaultConfig
+
+-- Change the below to your needs, following the explanation in comments. See
+-- module Yi.Config for more information on configuration. Other configuration
+-- examples can be found in the examples directory. You can also use or copy
+-- another user configuration, which can be found in modules Yi.Users.*
+
 main :: IO ()
 main = yi $ defaultConfig 
   {
-   -- Override the default UI as such: (can also be chosen on the command line)
-   -- startFrontEnd = Yi.UI.Gtk.start,
    
-   -- Keymap to use:
-   defaultKm = Yi.Keymap.Emacs.keymap,
-   configKillringAccumulate = True,    -- Should be True for emacs, False for others.
+   -- Keymap Configuration
+   defaultKm = Yi.Keymap.Emacs.keymap,   -- Replace Emacs by Vim or Cua as needed.
+   configKillringAccumulate = True,      -- Should be True for emacs, False for others.
 
-   -- UI options:
-   configUI = (configUI defaultConfig) 
+   -- UI Configuration
+   -- Override the default UI as such: 
+   startFrontEnd = startFrontEnd defaultConfig,
+                    -- Yi.UI.Vty.start -- for Vty
+   -- (can be overridden at the command line)
+   -- Options:
+   configUI = defaultUIConfig
      { 
-       configFontSize = Nothing        -- 'Just 10' for specifying the size.
-     -- , configStyle = darkBlueTheme  -- Change the color scheme here.
-     -- , configWindowFill = '~'       -- Typical for Vim
+       configFontSize = Nothing,
+                        -- 'Just 10' for specifying the size.
+       configStyle = configStyle defaultUIConfig,
+                     -- darkBlueTheme  -- Change the color scheme here.
+       
+       configWindowFill = ' ' 
+                          -- '~'    -- Typical for Vim
      }
   }
 
-  -- See the documentation of Yi.Config for more information on
-  -- configuration.  Other configuration examples can be found in the
-  -- examples directory; you can also use or copy another user
-  -- configuration, which can be found in modules Yi.Users.*
