81c7fa87a200d4745e997ed0c6a91f8926763f2d
jeanphilippe.bernardy@gmail.com <unknown>
2008-09-18 08:52:15 +0000

    ground layer should be painted in window style

--- Yi/Buffer/Implementation.hs
+++ Yi/Buffer/Implementation.hs
@@ -276,7 +276,7 @@ strokesRangesBI regex i j fb@FBufferData {hlCache = HLState hl cache} =  result
     dropBefore = dropWhile (\(_l,_s,r) -> r <= i)
     takeIn  = takeWhile (\(l,_s,_r) -> l <= j)
 
-    groundLayer = [(i,defaultStyle,j)]
+    groundLayer = [(i,window,j)]
     syntaxHlLayer = hlGetStrokes hl point i j $ hlGetTree hl cache
     layers2 = map (map overlayStroke) $ groupBy ((==) `on` overlayLayer) $  Set.toList $ overlays fb
     layer3 = case regex of 
