e97a3145be81be3e41448b6bec8f91144bf4ce44
gwern0@gmail.com <unknown>
2008-11-29 23:28:22 +0000

    Yi.Mode.IReader: simplify, rm useless cm setting, restore *.irtxt association

--- Yi/Mode/IReader.hs
+++ Yi/Mode/IReader.hs
@@ -3,26 +3,24 @@
 -- We have it as a separate mode so users can bind the commands to this mode specifically.
 module Yi.Mode.IReader where
 
-import Yi.Buffer.HighLevel
 import Yi.Buffer.Misc
 import Yi.IReader
 import Yi.Keymap.Keys
-import Yi.Modes
+import Yi.Modes (anyExtension, fundamentalMode)
 import Yi.Syntax (Stroke)
-import qualified Yi.Syntax.Linear as Linear
+import qualified Yi.Syntax.Linear as Linear (Result)
 
 abstract :: forall syntax. Mode syntax
-abstract = fundamentalMode { modeToggleCommentSelection = toggleCommentSelectionB "<!-- " "-->",
-                             modeApplies = anyExtension ["irtxt"],
+abstract = fundamentalMode { modeApplies = anyExtension ["irtxt"],
                              modeKeymap = ikeys }
+    where -- Default bindings.
+          -- ikeys :: (MonadInteract f Yi.Keymap.Action Event) => f () -> f ()
+          ikeys = (choice  [metaCh '1' ?>>! saveAndNextArticle,
+                            metaCh '2' ?>>! saveAsNewArticle,
+                            metaCh '3' ?>>! deleteAndNextArticle] <||)
 
 ireaderMode :: Mode (Linear.Result Stroke)
-ireaderMode = abstract { modeName = "text files for interactive reading" }
-
--- Default bindings.
-ikeys = (choice  [metaCh '1' ?>>! saveAndNextArticle,
-                  metaCh '2' ?>>! saveAsNewArticle,
-                  metaCh '3' ?>>! deleteAndNextArticle] <||)
+ireaderMode = abstract { modeName = "interactive reading of text" }
 
 ireadMode ::  BufferM ()
 ireadMode = setAnyMode (AnyMode ireaderMode)
