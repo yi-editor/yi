62451aa4fa9338648040264862599a28116e0fa3
gwern0@gmail.com <unknown>
2008-12-12 16:53:32 +0000

    Yi.Users.Gwern: switch to use shim-mode Bind a key to infer type, and do some formatting.

--- Yi/Users/Gwern.hs
+++ Yi/Users/Gwern.hs
@@ -1,30 +1,29 @@
--- import Yi.UI.Gtk (start)
--- import Yi.UI.Vty (start)
-
 import Yi 
 import Yi.Buffer.HighLevel
 import Yi.Buffer.Misc
 import Yi.Keymap.Emacs (keymap)
 import Yi.Mode.Haskell as Haskell
 import Yi.Mode.IReader as IReader
-
-defaultUIConfig :: UIConfig
-defaultUIConfig = configUI defaultConfig
+import Yi.Mode.Shim    as Shim
 
 main :: IO ()
 main = yi $ defaultConfig
-  { modeTable = AnyMode bestHaskellMode : AnyMode IReader.ireaderMode : modeTable defaultConfig,
+ { modeTable = AnyMode bestHaskellMode : AnyMode IReader.ireaderMode : modeTable defaultConfig,
 
    -- Keymap Configuration
-   defaultKm = Yi.Keymap.Emacs.keymap -- Override M-g g, for shorter M-g binding.
+    defaultKm = Yi.Keymap.Emacs.keymap -- Override M-g g, for shorter M-g binding.
                                 <|> (metaCh 'g' ?>>! gotoLn),
    configKillringAccumulate = True,      -- Should be True for emacs, False for others.
 
    -- UI Configuration
    -- Override the default UI as such:
    startFrontEnd = startFrontEnd defaultConfig,
-   configUI = defaultUIConfig {configFontSize = Nothing, configTheme = configTheme defaultUIConfig, configWindowFill = ' '}}
-    where -- bestHaskellMode :: Mode (Yi.Syntax.Paren.Expr (Yi.Lexer.Alex.Tok Yi.Lexer.Haskell.Token))
-          bestHaskellMode = Haskell.cleverMode { modeKeymap = (choice [ctrlCh 'c' ?>> ctrl (char 'l') ?>>! ghciLoadBuffer,
-                                                                       -- Use a more clever binding for Home
-                                                                       spec KHome ?>>! moveNonspaceOrSol] <||) }
+   configUI = defaultUIConfig {configFontSize = Nothing, configTheme = configTheme defaultUIConfig, 
+                               configWindowFill = ' '}}
+    where defaultUIConfig = configUI defaultConfig
+
+          bestHaskellMode = Shim.minorMode $ 
+           Haskell.cleverMode { modeKeymap = (choice [ctrlCh 'c' ?>> ctrl (char 'l') ?>>! ghciLoadBuffer,
+                                                      ctrlCh 'c' ?>> ctrl (char 't') ?>>! annotType,
+                                                      -- Use a more clever binding for Home
+                                                      spec KHome ?>>! moveNonspaceOrSol] <||) }
