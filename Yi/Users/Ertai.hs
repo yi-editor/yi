da29c6b3ccde7eb144616ac2a6935fa500fbacc1
Nicolas Pouillard <nicolas.pouillard@gmail.com>
2008-12-16 14:25:38 +0000

    ertai: update my config.

--- Yi/Users/Ertai.hs
+++ Yi/Users/Ertai.hs
@@ -1,12 +1,69 @@
--- This is ertai's Yi config
-
 import Yi
-
-import Yi (Keymap, (<|>))
-import Yi.Keymap.Keys (char, (?>>!))
+import qualified Yi.Mode.Haskell as Haskell
+import Yi.Prelude
+import Prelude (map)
+import Data.List (isPrefixOf, reverse, length)
+import Data.Maybe
+import Yi.Char.Unicode (greek, symbols)
+import Control.Monad (replicateM_)
+import Yi.Keymap.Keys (char,(?>>!),(>>!))
+import Yi.Misc (adjBlock)
+import Yi.Buffer
+import Yi.Keymap.Vim (viWrite, v_top_level, v_ins_char)
 import qualified Yi.Keymap.Vim as Vim
 
-keymap :: Keymap
-keymap = Vim.keymap <|> (char ',' ?>>! Vim.viWrite)
+myModetable :: [AnyMode]
+myModetable = [
+               AnyMode Haskell.cleverMode
+              ,
+               AnyMode Haskell.fastMode
+              ]
 
-main = yi $ defaultConfig {defaultKeymap = keymap}
+main :: IO ()
+main = yi $ defaultConfig { modeTable = fmap (onMode prefIndent) (myModetable ++ modeTable defaultConfig)
+                          , defaultKm = Vim.mkKeymap extendedVimKeymap
+                          }
+
+-- Set soft tabs of 4 spaces in width.
+prefIndent :: Mode s -> Mode s
+prefIndent m = m { modeIndentSettings = IndentSettings { expandTabs = True
+                                                       , shiftWidth = 2
+                                                       , tabSize = 2 }
+                 }
+
+mkInputMethod :: [(String,String)] -> Keymap
+mkInputMethod xs = choice [pString i >> adjustPriority (negate (length i)) >>! insertN o | (i,o) <- xs]
+
+extraInput :: Keymap
+extraInput = ctrl (char ']') ?>> mkInputMethod (greek ++ symbols)
+
+-- need something better
+unicodifySymbols :: BufferM ()
+unicodifySymbols = modifyRegionB f =<< regionOfB unitViWORD
+  where f x = fromMaybe x $ lookup x (greek ++ symbols)
+
+extendedVimKeymap :: Proto Vim.ModeMap
+extendedVimKeymap = Vim.defKeymap `override` \super self -> super
+    { v_top_level = (deprioritize >> v_top_level super)
+                    <|> (char ',' ?>>! viWrite)
+                    <|> ((events $ map char "|u") >>! unicodifySymbols)
+                    <|> ((events $ map char "\\c") >>! withModeB modeToggleCommentSelection)
+    , v_ins_char =
+            (deprioritize >> v_ins_char super)
+            -- I want softtabs to be deleted as if they are tabs. So if the
+            -- current col is a multiple of 4 and the previous 4 characters
+            -- are spaces then delete all 4 characters.
+            <|> (spec KBS ?>>! do
+                    c <- curCol
+                    line <- readRegionB =<< regionOfPartB Line Backward
+                    sw <- indentSettingsB >>= return . shiftWidth
+                    let indentStr = replicate sw ' '
+                        toDel = if (c `mod` sw) == 0 && indentStr `isPrefixOf` reverse line
+                                then sw
+                                else 1
+                    adjBlock (-toDel)
+                    replicateM_ toDel $ deleteB Character Backward
+                )
+            <|> (adjustPriority (-1) >> extraInput)
+    }
+
