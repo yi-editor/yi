7d8d0b351a00d6e7f08dc14d53353fe8816dc504
Nicolas Pouillard <nicolas.pouillard@gmail.com>
2008-12-21 10:34:06 +0000

    Yi.Hoogle: refactoring.

--- Yi/Hoogle.hs
+++ Yi/Hoogle.hs
@@ -2,47 +2,40 @@
 -- into a form useful for completion or insertion.
 module Yi.Hoogle where
 
-import Control.Monad (liftM)
+import Prelude ()
+import Control.Arrow ((&&&))
 import Data.Char (isUpper)
-import Data.List (isInfixOf, nub)
+import Data.List (isInfixOf, nub, filter, lines, words, map, (!!))
 
-import Yi hiding ((.), error)
+import Yi.Core
 import Yi.Process (runProgCommand)
 import Yi.Buffer (replaceRegionB, unitWord)
 
 -- | Remove anything starting with uppercase letter. These denote either module names or types.
 caseSensitize :: [String] -> [String]
-caseSensitize [] = []
-caseSensitize a = filter (\x -> not $ isUpper $ head x) a
+caseSensitize = filter (not . isUpper . head)
 
 -- | Hoogle's output includes a sort of type keyword, telling whether a hit is a package name, syntax,
 -- a module name, etc. But we care primarily about the function names, so we filter out anything containing
 -- the keywords.
 gv :: [String] -> [String]
-gv = filter (\x -> not $ "module " `isInfixOf` x || " type " `isInfixOf` x || "package " `isInfixOf` x || " data " `isInfixOf`  x || " keyword " `isInfixOf` x)
+gv = filter f
+  where f x = not $ any (`isInfixOf` x) ["module ", " type ", "package ", " data ", " keyword "]
 
 -- | Query Hoogle, with given search and options. This errors out on no results.
 hoogleRaw :: String -> String -> IO [String]
 hoogleRaw srch opts = do (out,_err,_status) <- runProgCommand "hoogle" [opts, srch]
                          let results = lines out
-                         if results == ["No results found"] then error "No Hoogle results" else
-                          return $ results
+                         if results == ["No results found"] then fail "No Hoogle results"
+                                                            else return results
 
 -- | Filter the output of 'hoogleRaw' to leave just functions.
 hoogleFunctions :: String -> IO [String]
-hoogleFunctions a = do let results = hoogleRaw a ""
-                       -- drop the module prefixes
-                       functions <- liftM (caseSensitize . gv . nub . map (\x -> (words x) !! 1)) results
-                       return functions
+hoogleFunctions a = caseSensitize . gv . nub . map ((!!1) . words) <$> hoogleRaw a ""
 
 -- | Return module-function pairs.
 hoogleFunModule :: String -> IO [(String, String)]
-hoogleFunModule a = do let results = hoogleRaw a ""
-                       clean <- liftM gv results                       
-                       let ws = map words clean
-                       let modules = map head ws
-                       let functions = (map $ \x -> x !! 1) ws
-                       return $ Prelude.zip modules functions
+hoogleFunModule a = (map (head &&& (!! 1)) . map words . gv) <$> hoogleRaw a ""
 
 -- | Call out to 'hoogleFunModule', and overwrite the word at point with
 -- the first returned function.
