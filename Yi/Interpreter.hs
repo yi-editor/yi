a0103f52c26b902d641f2f207eeaf8d4fdf753f0
jeanphilippe.bernardy@gmail.com <unknown>
2009-02-25 10:04:34 +0000

    experimental fiddling with a more typed interpreter

--- Yi/Interpreter.hs
+++ Yi/Interpreter.hs
@@ -17,6 +17,51 @@ import qualified Data.Map as M
 import Data.Traversable
 import Prelude hiding (mapM)
 import Data.Foldable
+import GHC.Base (Any)
+import Unsafe.Coerce
+
+type Var = String
+
+-- Big hurdle: supporting constraints.
+-- unsafeCoerce show ~~> ambiguous type variable. Thus we need a 
+data Type = Poly Var [Constraint] Type
+          | TyApp Type Type
+          | TyCon TyCon
+          | TyVar Var
+
+fromMono tr  = mkTyConApp' tc (fmap fromMono args)
+    where (tc,args) = splitTyConApp tr
+
+
+funTyCon = fst $ splitTyConApp $ typeOf (id :: Char -> Char)
+
+mkTyConApp' tyCon = foldl TyApp (TyCon tyCon) 
+
+mkFunTy t1 t2 = mkTyConApp funTyCon [t1,t2]
+
+
+-- funResultTy' t1 t2 = case unify t1 (mkFunTy t2 (TyVar "frt")) of
+funResultTy' = error "funResultTy'"
+
+
+
+data Constraint = Constraint TypeClass [Type]
+
+data TypeClass = Action
+
+-- | Polymorphic dynamic type
+data PolyDyn = PolyDyn Type Any 
+
+subst :: Var ->Type ->Type -> Type
+subst v s t0 = case t0 of
+    (Poly v' cs t) -> if v == v' then t0 else Poly v' (fmap substConstraint cs) (subst v s t)
+    (TyApp t1 t2) -> TyApp (subst v s t1) (subst v s t2)
+    (TyVar v') -> if v == v' then s else t0
+    _ -> t0
+    where substConstraint (Constraint tc ts) = Constraint tc (fmap (subst v s) ts)
+
+
+
 
 data Atom = AVar String
           | AString String
@@ -30,6 +75,7 @@ instance Show Atom where
     show (AChar s) = show s
 
 type Env = M.Map String [Dynamic]
+type Env' = M.Map String PolyDyn
 
 -- TODO: parens
 patom :: Parsec.CharParser st Atom
@@ -80,12 +126,44 @@ rename env = mapM renameOne
       renameOne (AInt x) = box x
       box x = return [toDyn x]
 
+rename' :: Env' -> UExpr Atom -> Either Err (UExpr PolyDyn)
+rename' env = mapM renameOne
+    where
+      renameOne (AVar v) = do
+        case M.lookup v env of
+          Just x -> Right x
+          Nothing -> Left $ v ++ " not found in the environment"
+      renameOne (AChar x) = box x
+      renameOne (AString x) = box x
+      renameOne (AInt x) = box x
+      box :: Typeable a => a -> Either err PolyDyn
+      box = Right . polyBox
+
+polyBox :: Typeable a => a -> PolyDyn
+polyBox x = PolyDyn (fromMono $ typeOf x) (unsafeCoerce x)
+
+
 interpret :: UExpr [Dynamic] -> Either Err [Dynamic]
 interpret (UVal da) = return $ da
 interpret (UApp df da) = do
   t1 <- interpret df
   t2 <- interpret da
   return $ catMaybes [dynApply f a | f <- t1, a <- t2]
+
+
+dynApply' (PolyDyn t1 f) (PolyDyn t2 x) =
+  case funResultTy' t1 t2 of
+    Just t3 ->Just (PolyDyn t3 ((unsafeCoerce f) x))
+    Nothing ->Nothing
+
+
+interpret' (UVal da) = return $ da
+interpret' (UApp df da) = do
+  f <- interpret' df
+  a <- interpret' da
+  dynApply' f a
+
+
 
 toMono :: forall a. Typeable a => [Dynamic] -> Either Err a
 toMono rs = case catMaybes $ map fromDynamic rs of
@@ -105,3 +183,4 @@ toMono rs = case catMaybes $ map fromDynamic rs of
 --    where 
 --         symTable = M.fromList [("one", [toDyn (1::Int)]),
 --                                ("show",[toDyn (show :: Int -> String)]) ]
+
