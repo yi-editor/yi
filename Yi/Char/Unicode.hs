903dd3d7ebd3754cfa146e34dfc9ed0dee9febd6
jeanphilippe.bernardy@gmail.com <unknown>
2009-03-10 09:20:30 +0000

    normalization work on the Unicode input (greek)

--- Yi/Char/Unicode.hs
+++ Yi/Char/Unicode.hs
@@ -57,6 +57,42 @@ greek = [("alpha", "α")
         ,("'O", "Ω")
         ]
 
+greekData = [(Just 'a', "alpha", "α")
+            ,(Just 'b', "beta", "β")
+            ,(Just 'g', "gamma", "γ")
+            ,(Just 'G', "Gamma", "Γ")
+            ,(Just 'd', "delta", "δ")
+            ,(Just 'D', "Delta", "Δ")
+            ,(Nothing , "epsilon", "ε")
+            ,(Just 'z', "zeta", "ζ")
+            ,(Nothing , "eta", "η")
+            ,(Nothing , "theta", "θ")
+            ,(Nothing , "Theta", "Θ")
+            ,(Just 'i', "iota", "ι")
+            ,(Just 'k', "kapa", "κ")
+            ,(Just 'l', "lambda", "λ")
+            ,(Just 'L', "Lambda", "Λ")
+            ,(Just 'm', "mu", "μ")
+            ,(Just 'n', "nu", "ν")
+            ,(Just 'x', "xi", "ξ")
+            ,(Just 'o', "omicron", "ο")
+            ,(Nothing , "pi", "π")
+            ,(Nothing , "Pi", "Π")
+            ,(Just 'r', "rho", "ρ")
+            ,(Just 's', "sigma", "σ")
+            ,(Just 'S', "Sigma", "Σ")
+            ,(Just 't', "tau", "τ")
+            ,(Nothing , "phi", "φ")
+            ,(Nothing , "Phi", "Φ")
+            ,(Nothing , "chi", "χ")
+            ,(Nothing , "Chi", "Χ")
+            ,(Nothing , "psi", "ψ")
+            ,(Nothing , "Psi", "Ψ")
+            ,(Just 'w', "omega", "ω")
+            ,(Just 'O', "Omega", "Ω")
+            ]
+
+
 symbols :: [(String, String)]
 symbols =
  [
