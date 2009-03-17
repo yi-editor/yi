2f616d390792f90ef9f149bd2841e426b5e2bf22
kq1quick@gmail.com <unknown>
2009-03-17 23:54:11 +0000

    Supply helpful env-var information on GTK/Pango icon file load errors.

--- Yi/UI/Gtk/Utils.hs
+++ Yi/UI/Gtk/Utils.hs
@@ -9,9 +9,12 @@ module Yi.UI.Gtk.Utils where
 import Paths_yi
 import System.FilePath
 import Graphics.UI.Gtk
+import System.Glib.GError
 
 loadIcon :: FilePath -> IO Pixbuf
 loadIcon fpath = do
   datadir <- getDataDir
-  icoProject <- pixbufNewFromFile (datadir </> "art" </> fpath)
+  icoProject <- catchGError (pixbufNewFromFile (datadir </> "art" </> fpath))
+                            (\(GError dom code msg) -> throwGError $ GError dom code $
+                             msg ++ " -- use the yi_datadir environment variable to specify an alternate location")
   pixbufAddAlpha icoProject (Just (0,255,0))
