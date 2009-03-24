bbc1a3f21f55660f27d9bcb485c04c1e2dfa45c3
andekar@student.chalmers.se <unknown>
2009-03-24 14:53:51 +0000

    ParserTest.hs: Added functionality to test parsing of javascripts

--- ParserTest.hs
+++ ParserTest.hs
@@ -11,72 +11,95 @@ import System.FilePath.Posix
 import System.Directory
 import Control.Monad
 import qualified Data.Foldable as P
-import Data.List
+import Data.List as List
 import qualified Yi.Lexer.Alex as Alex
 import Data.Time
 
-haskellLexer = Alex.lexScanner alexScanToken initState 
+haskellLexer = Alex.lexScanner alexScanToken initState
+jsLexer = Alex.lexScanner JSLex.alexScanToken JSLex.initState
 
 main = do
     arg <- getArgs
     let (flags,rest) = partition (isPrefixOf "-") arg  -- flag begins with -
     case arg of
-        [] -> putStrLn $ "-js for javascript\n-hs for haskell\n-d for debug mode\n" ++ 
+        [] -> putStrLn $ "-js for javascript\n-hs for haskell\n-d for debug mode\n" ++
                          "-r for recursive\notherwise a list of files/folders...\n" ++
                          "-cmp to compare with Paren.hs parse\n-oneBy to parse one token at a time"
         _ -> mapM_ (dirs flags) rest
 
+-- | Find files to parse, recursively if flagged -r
 dirs flags dir = do
     b <- doesDirectoryExist dir
-    let b' = b &&(Data.List.all ((/=) '.') dir)
+    let b' = b &&(List.all ((/=) '.') dir) -- dont look in "." or ".."
     case b' of
         True -> (pFiles =<< getFiles) >> recurse
-        False -> pFiles [dir]
-  where recurse = if (Data.List.elem "-r" flags) then ((\x -> mapM_ (dirs flags) x) =<< filterM (doesDirectoryExist) 
+        False -> pFiles [dir] -- check so that file has correct extension
+  where recurse = if (List.elem "-r" flags) then ((\x -> mapM_ (dirs flags) x) =<< filterM (doesDirectoryExist) 
                       =<< liftM2 map (return $ (</>) dir) (getDirectoryContents dir)) else return ()
-        getExt = if (Data.List.elem "-hs" flags) then ".hs" else ".js"
-        getFiles = (filterM doesFileExist . filter ((==) getExt . takeExtension)
+        getExt = if (List.elem "-hs" flags) then [".hs"] else [".js",".json"] -- Should take more js files
+        getFiles = (filterM doesFileExist . filter (\x -> elem (takeExtension x) getExt )
                    . map ((</>) dir) =<< getDirectoryContents dir)
         pFiles arg' = do
             b <-P.foldlM (\a b -> liftM2 (==) (return a) (doesFileExist b)) True arg'
             case b of
                 True -> do
                     input <- mapM readFile arg'
-                    Control.Monad.mapM_ (fParse flags) $ zip arg' input
-                False -> putStrLn $ (++) "List contains non existing file" (show arg')
+                    Control.Monad.mapM_ (parse flags) $ zip arg' input
+                False -> return () -- not valid file/folder
 
-fParse flags (fName, input) = do
-    if (Data.List.elem "-cmp" flags) then -- compare with Paren.hs
-       print =<< (liftM2 (-) (mParse (mkProcess Haskell.parse) Haskell.indentScanner) (mParse (mkProcess Paren.parse) Paren.indentScanner))
-       else mParse (mkProcess Haskell.parse) Haskell.indentScanner >> return () -- otherwise just run the parser
-  where 
-      mParse proc scan = do
-           let allSyms = getSyms scan
-           time <- getCurrentTime
-           let finalResult = option proc allSyms
-           putStrLn $ fName ++ " # " ++ (show $ evalL finalResult)
-           time' <- getCurrentTime
-           if (Data.List.elem "-d" flags) then do
-               putStrLn $ "Shifts: " ++ (show $ shifts finalResult)
-               putStrLn $ "Disjunctions: " ++ (show $ best finalResult)
-               print allSyms
-               return $ diffUTCTime time' time
-               else return $ diffUTCTime time' time
-      getSyms p = 
+-- | Parse given flags, file name and content
+parse flags (fName, input) = do
+    if (List.elem "-cmp" flags) then do -- compare with Paren.hs
+--        t <-getCurrentTime
+       let tokList = getSyms (Haskell.indentScanner . haskellLexer)
+           (paths, finRes) = option (mkProcess Haskell.parse) tokList
+--        putStrLn $ "Haskell: " ++ fName ++ " # " ++ (show $ evalL finRes)
+--        t' <- getCurrentTime
+       let tokList' = getSyms (Paren.indentScanner . haskellLexer)
+           (paths', finRes') = option (mkProcess Paren.parse) tokList'
+--        putStrLn $ "Paren:   " ++ fName ++ " # " ++ (show $ evalL finRes')
+--        tt' <- getCurrentTime
+--        print $ (-) (diffUTCTime t' t) (diffUTCTime tt' t')
+       debug $ getSyms (Haskell.indentScanner . haskellLexer)
+       mapM_ print (zip3 paths paths' tokList)
+       putStrLn $ "Different paths (Haskell,Paren): " ++ (show $ zip paths paths')
+       putStrLn $ "Haskell: " ++ fName ++ " # " ++ (show $ evalL finRes)
+       putStrLn $ "Paren:   " ++ fName ++ " # " ++ (show $ evalL finRes')
+       else if (List.elem "-hs" flags) then do
+                 let tokList = getSyms (Haskell.indentScanner . haskellLexer)
+                     (r,info) = option (mkProcess Haskell.parse) tokList -- otherwise just run the parser
+                 mapM_ print r
+                 putStrLn $ show $ evalL info
+                 else do
+                     let tokList = getSyms jsLexer
+                         (r,info) = option (mkProcess JS.parse) tokList
+                     mapM_ print (zip r tokList) -- will print width followed by token added
+                     putStrLn $ show $ evalL info
+  where
+      debug allSyms = if (List.elem "-d" flags) then
+                           mapM_ print allSyms
+                           else return ()
+      getSyms scan =
           let getText = Scanner 0 id (error "getText: no character beyond eof")
                          (\idx -> zip [idx..] (drop (fromIntegral idx) input))
-              scan = (p . haskellLexer) getText        
-          in map snd $ scanRun scan $ scanInit scan
-      option thisFar xs = if (Data.List.elem "-oneBy" flags) then -- Choose to go stepwise or all in
+              scan' = scan getText
+          in map snd $ scanRun scan' $ scanInit scan'
+      option thisFar xs = if (List.elem "-oneBy" flags) then -- Choose to go stepwise or all in
                               oneByOne thisFar xs
-                              else pushEof $ pushSyms xs $ thisFar
+                              else ([],pushEof $ pushSyms xs $ thisFar)
 
-oneByOne thisFar [] = pushEof thisFar
-oneByOne thisFar (h:rest) = do
+-- | Push the symbols into the parser one by one
+-- Send around the different number of (Best)
+oneByOne thisFar [] = let p = pushEof thisFar in ([], p)
+oneByOne thisFar (h:rest) = 
     let res = pushSyms [h] thisFar
-    let eRes = oneByOne res []
-    oneByOne res rest -- TODO: add check for errors and stop then?
+        eRes = evalL res
+        (i, p') = oneByOne eRes rest
+    in  ((countWidth res):i,p')
 
-shifts s = length $ filter ('>' ==) (show s)
+-- best s = length $ filter ('^' ==) (show s)
 
-best s = length $ filter ('(' ==) (show s)
+best s = List.foldl (\p q -> p + value q) 0 (show s)
+
+value '^' = 1
+value _ = 0
