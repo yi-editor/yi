4efe62d8f14a932257df0f8df69c10e7f1bfe242
gwern0@gmail.com <unknown>
2009-04-12 16:41:17 +0000

    fix Yi.Users.Gwern

--- Yi/Users/Gwern.hs
+++ Yi/Users/Gwern.hs
@@ -2,27 +2,17 @@ import Yi  hiding ((.))
 import Yi.Buffer.HighLevel
 import Yi.Buffer.Misc
 import Yi.Hoogle (hoogle)
-import Yi.Keymap.Emacs (keymap)
+import Yi.Keymap.Emacs (defKeymap, eKeymap, mkKeymap)
 import Yi.Misc
 import Yi.Mode.Haskell as Haskell
 import Yi.Mode.IReader as IReader
 import Yi.Mode.Shim    as Shim
 
-myKeymap :: KeymapSet
-myKeymap = mkKeymap $ override Yi.Keymap.Emacs.defKeymap $ \proto _self -> 
-   proto {
-           eKeymap = eKeymap proto
-                                ||> (metaCh 'g' ?>>! gotoLn),
-         }
-
-
 main :: IO ()
 main = yi $ defaultEmacsConfig
  { modeTable = AnyMode bestHaskellMode : AnyMode IReader.ireaderMode : modeTable defaultConfig,
-
    -- Keymap Configuration
-    defaultKm = myKeymap
-
+   defaultKm = myKeymap,
    configUI = defaultUIConfig {configFontSize = Nothing, configWindowFill = ' '}}
     where defaultUIConfig :: UIConfig
           defaultUIConfig = configUI defaultConfig
@@ -30,8 +20,8 @@ main = yi $ defaultEmacsConfig
           bestHaskellMode = Shim.minorMode $ 
            Haskell.cleverMode { modeKeymap = 
                                 (choice [ctrlCh 'c' ?>> ctrl (char 'l') ?>>! ghciLoadBuffer,
-                                         -- Insert a type, go back up a line, and indent it properly
-                                         -- good for 'where' expressions
+                                         -- Insert a type, go back up a line, and indent it properly.
+                                         -- Good for 'where' expressions
                                          ctrlCh 'c' ?>> ctrl (char 't') ?>>! (annotType 
                                                                               >> (withBuffer $ moveB VLine Backward 
                                                                                   >> adjIndent IncreaseCycle)),
@@ -39,3 +29,8 @@ main = yi $ defaultEmacsConfig
                                          ctrlCh 'c' ?>> ctrl (char 'h') ?>>! hoogle,
                                          -- Use a more clever binding for Home
                                          spec KHome ?>>! moveNonspaceOrSol] <||) }
+
+-- Add a M-g binding for goto-line
+myKeymap :: KeymapSet
+myKeymap = mkKeymap $ override Yi.Keymap.Emacs.defKeymap $ \proto _self -> 
+   proto { eKeymap = eKeymap proto ||> (metaCh 'g' ?>>! gotoLn) }
