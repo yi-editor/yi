6358a06f50898e597f4b0dbf322526cba47e68dd
andekar@student.chalmers.se <unknown>
2009-06-18 12:46:40 +0000

    Mode/Haskell.hs: more indentation work

--- Yi/Mode/Haskell.hs
+++ Yi/Mode/Haskell.hs
@@ -246,6 +246,7 @@ cleverAutoIndentHaskellC' e behaviour = do
           Just _ -> previousIndent : maybe 0 firstTokOnCol (getFirstElement exp) : stopsOf r ++ stopsOf ts'
       stopsOf ((Hask.Expr e):ts) = stopsOf e ++ stopsOf ts
       stopsOf ((Hask.TS _ _):ts') = stopsOf ts'
+      stopsOf ((Hask.PError _ _ _):ts') = stopsOf ts'
       stopsOf [] = []
       stopsOf (r:_) = error (show r) -- stopsOf ts -- not yet handled stuff
        -- calculate indentation of operator (must be at least 1 to be valid)
