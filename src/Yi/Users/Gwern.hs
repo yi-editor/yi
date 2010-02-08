9cb9be84c840d3e48694fa698de522a65e1b6785
gwern0@gmail.com <unknown>
2010-02-08 21:41:42 +0000

    update my config to compile with darcs yi

--- src/Yi/Users/Gwern.hs
+++ src/Yi/Users/Gwern.hs
@@ -1,36 +1,31 @@
-import Yi  hiding ((.))
-import Yi.Buffer.HighLevel
-import Yi.Buffer.Misc
+import Yi
 import Yi.Hoogle (hoogle)
 import Yi.Keymap.Emacs (defKeymap, eKeymap, mkKeymap)
-import Yi.Misc
-import Yi.Mode.Haskell as Haskell
-import Yi.Mode.IReader as IReader
-import Yi.Mode.Shim    as Shim
+import qualified Yi.Mode.Haskell as H
+import qualified Yi.Mode.IReader as IReader (ireaderMode)
 
 main :: IO ()
 main = yi $ defaultEmacsConfig
  { modeTable = AnyMode bestHaskellMode : AnyMode IReader.ireaderMode : modeTable defaultConfig,
    -- Keymap Configuration
    defaultKm = myKeymap,
-   configUI = defaultUIConfig {configFontSize = Nothing, configWindowFill = ' '}}
+   configUI = defaultUIConfig {configFontSize = Nothing, configWindowFill = ' '} }
     where defaultUIConfig :: UIConfig
           defaultUIConfig = configUI defaultConfig
+          -- bestHaskellMode :: Mode (Tree (Tok Token))
+          bestHaskellMode = -- Shim.minorMode $
+                             H.cleverMode { modeKeymap =
+                                    topKeymapA ^: ((ctrlCh 'c' ?>> choice [ctrlCh 'l' ?>>! H.ghciLoadBuffer,
+                                                              ctrl (char 'z') ?>>! H.ghciGet,
+                                                              ctrl (char 'h') ?>>! hoogle,
+                                                              ctrlCh 'r' ?>>! H.ghciSend ":r",
+                                                              ctrlCh 't' ?>>! H.ghciInferType])
+                                                   <||) }
 
-          bestHaskellMode = Shim.minorMode $ 
-           Haskell.cleverMode { modeKeymap = 
-                                (choice [ctrlCh 'c' ?>> ctrl (char 'l') ?>>! ghciLoadBuffer,
-                                         -- Insert a type, go back up a line, and indent it properly.
-                                         -- Good for 'where' expressions
-                                         ctrlCh 'c' ?>> ctrl (char 't') ?>>! (annotType 
-                                                                              >> (withBuffer $ moveB VLine Backward 
-                                                                                  >> adjIndent IncreaseCycle)),
-                                         -- Complete the word at point with Hoogle
-                                         ctrlCh 'c' ?>> ctrl (char 'h') ?>>! hoogle,
-                                         -- Use a more clever binding for Home
-                                         spec KHome ?>>! moveNonspaceOrSol] <||) }
-
--- Add a M-g binding for goto-line
 myKeymap :: KeymapSet
-myKeymap = mkKeymap $ override Yi.Keymap.Emacs.defKeymap $ \proto _self -> 
-   proto { eKeymap = eKeymap proto ||> (metaCh 'g' ?>>! gotoLn) }
+myKeymap = mkKeymap $ override Yi.Keymap.Emacs.defKeymap $ \proto _self ->
+   proto { eKeymap = eKeymap proto ||>
+           -- Add a M-g binding for goto-line
+           (metaCh 'g' ?>>! gotoLn)
+           -- Use a more clever binding for Home
+           ||> (spec KHome ?>>! moveNonspaceOrSol) }
