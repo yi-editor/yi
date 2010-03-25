a7b2b62801be62fa6eef4e9b33310eb717c1f7e8
jeffwheeler@gmail.com <unknown>
2010-03-25 05:30:35 +0000

    -Wall (RecursiveDo -> DoRec, requires 6.12)

--- src/Yi/Core.hs
+++ src/Yi/Core.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE ScopedTypeVariables, RecursiveDo, Rank2Types #-}
+{-# LANGUAGE ScopedTypeVariables, DoRec, Rank2Types #-}
 
 -- Copyright (c) Tuomo Valkonen 2004.
 -- Copyright (c) Don Stewart 2004-5. http://www.cse.unsw.edu.au/~dons
@@ -113,13 +113,14 @@ startEditor cfg st = do
 
     -- Setting up the 1st window is a bit tricky because most functions assume there exists a "current window"
     newSt <- newMVar $ YiVar editor [] 1 M.empty
-    (ui, runYi) <- mdo let handler exception = runYi $ (errorEditor (show exception) >> refreshEditor)
-                           inF  ev  = handle handler (runYi (dispatch ev))
-                           outF acts = handle handler (runYi (interactive acts))
-                       ui <- uiStart cfg inF outF editor
-                       let runYi f = runReaderT (runYiM f) yi
-                           yi = Yi ui inF outF cfg newSt 
-                       return (ui, runYi)
+    (ui, runYi) <-
+      do rec let handler exception = runYi $ errorEditor (show exception) >> refreshEditor
+                 inF ev    = handle handler $ runYi $ dispatch ev
+                 outF acts = handle handler $ runYi $ interactive acts
+                 runYi f   = runReaderT (runYiM f) yi
+                 yi        = Yi ui inF outF cfg newSt
+             ui <- uiStart cfg inF outF editor   
+         return (ui, runYi)
   
     runYi $ do if isNothing st
                     then postActions $ startActions cfg -- process options if booting for the first time
