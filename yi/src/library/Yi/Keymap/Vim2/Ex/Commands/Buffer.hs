50b0bb79f89b6878123376b4dc03cb1d7d8bc810
Ben Armston <ben.armston@googlemail.com>
2014-01-03 23:11:03 +0000

    Add :buffer Ex command
    Mostly compatible with Vim. The incompatibilities are
    
     - There must be a space between the command and the buffer reference, even if
       that reference is a number. E.g., vim supports `:b4` but this command
       requires that to be `:b 4`.
    
     - A trailing `!` causes the command to not be recognized.
    
     - The buffer command behaves as if a trailing `!` had been added.
    
     - There is no completion support.

---
+++ yi/src/library/Yi/Keymap/Vim2/Ex/Commands/Buffer.hs
@@ -0,0 +1,52 @@
+-- :buffer ex command to switch to named or numbered buffer.
+module Yi.Keymap.Vim2.Ex.Commands.Buffer
+    ( parse
+    ) where
+
+import Prelude ()
+import Yi.Prelude
+
+import Data.Maybe 
+import qualified Text.ParserCombinators.Parsec as P
+
+import Yi.Editor
+import Yi.Buffer.Misc
+import Yi.Buffer.Basic
+import Yi.Keymap
+import Yi.Keymap.Vim2.Ex.Types
+import qualified Yi.Keymap.Vim2.Ex.Commands.Common as Common
+
+parse :: String -> Maybe ExCommand
+parse = Common.parse $ do
+    -- TODO bufIdent and the spaces should be optional, in which
+    --      case the command becomes a noop just like for the empty string.
+    -- TODO If bufIdent is a number the spaces should be optional.
+    -- TODO If no bufIdent is given and the buffer is modified and 'hidden'
+    --      is not set, should this operation be a noop?
+    void $ P.try ( P.string "buffer") <|> P.try ( P.string "b")
+    bufIdent <- P.many1 P.space *> P.many P.anyChar
+    return $ Common.pureExCommand {
+        cmdShow = "buffer"
+      , cmdAction = EditorA $ switchToBuffer bufIdent 
+      }
+
+
+switchToBuffer :: String -> EditorM ()
+switchToBuffer s = do
+    case P.parse bufferRef "" s of
+        Right ref -> switchByRef ref
+        Left e    -> switchByName s
+  where
+    bufferRef = BufferRef . read <$> P.many1 P.digit
+
+
+switchByName :: String -> EditorM ()
+switchByName ""      = return ()
+switchByName "#"     = switchToBufferWithNameE "" 
+switchByName bufName = switchToBufferWithNameE bufName 
+
+
+switchByRef :: BufferRef -> EditorM () 
+switchByRef ref = do
+    mBuf <- findBuffer ref
+    maybe (return ()) (switchToBufferE . bkey) mBuf
