1f6a9bb81dc23921ee8da50bf98f5c99927b0f01
Michal J. Gajda <mjgajda@gmail.com>
2014-02-11 17:53:56 +0100

    My current config.

--- yi-contrib/src/Yi/Config/Users/Michal.hs
+++ yi-contrib/src/Yi/Config/Users/Michal.hs
@@ -1,19 +1,130 @@
-import Yi
-
--- Preamble
-import Yi.Prelude
 import Prelude (take, length, repeat)
-import Yi.Keymap.Vim(mkKeymap,defKeymap,ModeMap(..))
+import Data.List(isPrefixOf, isSuffixOf)
+import System.FilePath(takeFileName)
+import Yi
+import Yi.Keymap.Vim
+import qualified Yi.Keymap.Vim2 as V2
+import qualified Yi.Keymap.Vim2.Common as V2
+import qualified Yi.Keymap.Vim2.Utils as V2
 
+import qualified Yi.Mode.Haskell as Haskell
+
+-- Used in Theme declaration
+import Data.Monoid(mappend)
+import qualified Yi.Style(Color(Default))
+
+-- Used for inserting current date
+--import System.Time(getTimeOfDay)
 import Data.Time.Clock(getCurrentTime)
 import System.Locale(defaultTimeLocale)
 import Data.Time.Format(formatTime)
 
-import Yi.UI.Vty (start)
-import Yi.UI.Pango (start)
+main :: IO ()
+main = yi $ defaultVimConfig {
+    modeTable = myModes ++ fmap (onMode prefIndent) (modeTable defaultVimConfig),
+    defaultKm = myKeymapSet,
+    configCheckExternalChangesObsessively = False,
+    configUI = (configUI defaultVimConfig)
+     { 
+       configTheme = myTheme,       
+       configWindowFill = '~'    -- Typical for Vim
+     }
+}
 
--- Used in Theme declaration
-import Data.Monoid(mappend)
+defaultSearchKeymap :: Keymap
+defaultSearchKeymap = do
+    Event (KASCII c) [] <- anyEvent
+    write (isearchAddE [c])
+
+myKeymapSet :: KeymapSet
+myKeymapSet = V2.mkKeymapSet $ V2.defVimConfig `override` \super this ->
+    let eval = V2.pureEval this
+    in super {
+          -- Here we can add custom bindings.
+          -- See Yi.Keymap.Vim2.Common for datatypes and 
+          -- Yi.Keymap.Vim2.Utils for useful functions like mkStringBindingE
+
+          -- In case of conflict, that is if there exist multiple bindings
+          -- whose prereq function returns WholeMatch,
+          -- the first such binding is used.
+          -- So it's important to have custom bindings first.
+          V2.vimBindings = myBindings eval ++ V2.vimBindings super
+        }
+
+myBindings :: (String -> EditorM ()) -> [V2.VimBinding]
+myBindings eval =
+    let nmap  x y = V2.mkStringBindingE V2.Normal V2.Drop (x, y, id)
+        imap  x y = V2.VimBindingE (\evs state -> case V2.vsMode state of
+                                    V2.Insert _ -> evs `V2.matchesString` x
+                                    _ -> V2.NoMatch)
+                                  (\_evs -> y >> return V2.Continue)
+        nmap'  x y = V2.VimBindingY prereq (\_ -> y >> return V2.Drop)
+          where prereq _ vs | V2.vsMode vs /= V2.Normal = V2.NoMatch
+                prereq evs _ = evs `V2.matchesString` "D"
+    in [
+         -- Tab traversal
+         nmap  "<C-h>" previousTabE
+       , nmap  "<C-l>" nextTabE
+       , nmap  "<C-l>" nextTabE
+
+         -- Press space to clear incremental search highlight
+       , nmap  " " (eval ":nohlsearch<CR>")
+
+         -- for times when you don't press shift hard enough
+       , nmap  ";" (eval ":")
+
+       , nmap  "<F3>" (withBuffer0 deleteTrailingSpaceB)
+       , nmap  "<F4>" (withBuffer0 moveToSol)
+       , nmap  "<F1>" (withBuffer0 readCurrentWordB >>= printMsg)
+
+       , imap  "<Home>" (withBuffer0 moveToSol)
+       , imap  "<End>"  (withBuffer0 moveToEol)
+       , nmap' "<D>"    insertCurrentDate
+       ]
+
+-- | I declare "proper black" in GTK, since Vty terminal seems to have grayish black in 16-color system.
+--   Fortunately default _background_ color is available through Default :: Yi.Style.Color.
+--   Note that this works only in background! (Default foreground color is green.)
+defaultColor :: Yi.Style.Color
+defaultColor = Yi.Style.Default
+
+-- This is based on Vim's ':colorscheme murphy', but with gray strings, and more brown on operators.
+myTheme = defaultTheme `override` \super _ -> super
+  { modelineAttributes   = emptyAttributes { foreground = black,   background = darkcyan           }
+  , tabBarAttributes     = emptyAttributes { foreground = white,   background = defaultColor            }
+  , baseAttributes       = emptyAttributes { foreground = defaultColor, background = defaultColor, bold=True }
+  , commentStyle         = withFg darkred `mappend` withBd False `mappend` withItlc True
+--  , selectedStyle        = withFg black   `mappend` withBg green `mappend` withReverse True
+  , selectedStyle        = withReverse True
+  , errorStyle           = withBg red     `mappend` withFg white
+  , operatorStyle        = withFg brown   `mappend` withBd False
+  , hintStyle            = withBg brown   `mappend` withFg black
+  , importStyle          = withFg blue
+  , dataConstructorStyle = withFg blue
+  , typeStyle            = withFg blue
+  , keywordStyle         = withFg yellow
+  , builtinStyle         = withFg brown
+  , strongHintStyle      = withBg brown   `mappend` withUnderline True
+  , stringStyle          = withFg brown   `mappend` withBd False
+  , preprocessorStyle    = withFg blue
+--  , constantStyle      = withFg cyan
+--  , specialStyle      = withFg yellow
+  }
+
+-- Softtabs of 2 characters for Berkeley coding style, if not editing makefile.
+prefIndent :: Mode syntax -> Mode syntax
+prefIndent m = if modeName m == "Makefile"
+                 then m
+                 else m {
+        modeIndentSettings = IndentSettings
+            {
+                expandTabs = True,
+                shiftWidth = 2,
+                tabSize    = 2
+            }
+        }
+
+myModes = [diaryMode]
 
 -- inserting current date and underline
 currentDate :: IO String
@@ -36,87 +147,13 @@ insertCurrentDate :: YiM ()
 insertCurrentDate = do d <- withUI (\_ -> currentDateAndUnderline)
                        withBuffer (insertN d)
 
-extendedVimKeymap = defKeymap `override` \super self -> super
-    {
-        v_top_level = 
-            (deprioritize >> v_top_level super)
-            <|> (ctrlCh 'd' ?>>! insertCurrentDate),
-            -- On 'o' in normal mode I always want to use the indent of the previous line.
-            -- TODO: If the line where the newline is to be inserted is inside a
-            -- block comment then the block comment should be "continued"
-            -- TODO: Ends up I'm trying to replicate vim's "autoindent" feature. This 
-            -- should be made a function in Yi.
-        v_ins_char = 
-            (deprioritize >> v_ins_char super) 
-            -- On starting to write a block comment I want the close comment 
-            -- text inserted automatically.
-            <|> choice 
-                [ pString open_tag >>! do
-                    insertN $ open_tag ++ " \n" 
-                    indentAsPreviousB
-                    insertN $ " " ++ close_tag
-                    lineUp
-                 | (open_tag, close_tag) <- 
-                    [ ("{-", "-}") -- Haskell block comments
-                    , ("/*", "*/") -- C++ block comments
-                    ]
-                ]
-    }
-
--- This is based on Vim's ':colorscheme murphy', but with gray strings, and more brown on operators.
-
-myTheme = defaultTheme `override` \super _ -> super
-  { modelineAttributes   = emptyAttributes { foreground = black, background = darkcyan         }
-  , tabBarAttributes     = emptyAttributes { foreground = white, background = black            }
-  , baseAttributes       = emptyAttributes { foreground = green, background = black, bold=True }
-  , commentStyle         = withFg darkred `mappend` withBd False `mappend` withItlc True
---  , selectedStyle        = withFg black   `mappend` withBg green `mappend` withReverse True
-  , selectedStyle        = withReverse True
-  , errorStyle           = withBg red     `mappend` withFg white
-  , operatorStyle        = withFg brown   `mappend` withBd False
-  , hintStyle            = withBg brown   `mappend` withFg black
-  , importStyle          = withFg blue
-  , dataConstructorStyle = withFg blue
-  , typeStyle            = withFg blue
-  , keywordStyle         = withFg yellow
-  , builtinStyle         = withFg brown
-  , strongHintStyle      = withBg brown   `mappend` withUnderline True
-  , stringStyle          = withFg brown   `mappend` withBd False
-  , preprocessorStyle    = withFg blue
---  , constantStyle      = withFg cyan
---  , specialStyle      = withFg yellow
-  }
-
--- Softtabs of 2 characters for Berkeley coding style, if not editing makefile.
-prefIndent :: Mode s -> Mode s
-prefIndent m = if modeName m == "Makefile"
-                 then m
-                 else
-      m {
-        modeIndentSettings = IndentSettings
-            {
-                expandTabs = True,
-                shiftWidth = 2,
-                tabSize    = 2
-            }
-        }
-
-
-defaultUIConfig = configUI defaultVimConfig
-
-main :: IO ()
-main = yi $ defaultVimConfig
-  {
-   -- Keymap Configuration
-   defaultKm = mkKeymap extendedVimKeymap,
-   modeTable = fmap (onMode $ prefIndent) (modeTable defaultVimConfig),
-
-   -- Options:
-   configUI = defaultUIConfig
-     { 
-       configTheme = myTheme,       
-       configWindowFill = ' ' 
-                          -- '~'    -- Typical for Vim
-     }
-  }
-
+-- NOTE: use fundamentalMode as a base?
+diaryMode :: AnyMode
+diaryMode = AnyMode $ (\super -> super { modeApplies = \path _contents -> let name = takeFileName path
+                                                                in ".txt" `isSuffixOf`  name &&
+                                                                   "diary" `isPrefixOf` name,
+                               modeName    = "Diary",
+                               modeOnLoad  = do modeOnLoad super
+                                                r:_ <- regexB Forward $ makeSimpleSearch "*** TODAY ***"
+                                                moveTo . regionStart $ r
+                             }) $ emptyMode
