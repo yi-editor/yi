5c0cc2e4400d5dd287564a3eb23af0acab89af46
Ben Armston <ben.armston@googlemail.com>
2014-03-17 01:25:01 +0000

    Rework setup code to more easily support more tests
    Instead of returning details only about one of the three created buffers,
    return details about all three.

--- yi/src/tests/Vim2/EditorManipulations/BufferExCommand.hs
+++ yi/src/tests/Vim2/EditorManipulations/BufferExCommand.hs
@@ -11,15 +11,24 @@ import Yi.Config (Config)
 
 import Generic.TestUtils
 
+type BufferName = String
 
--- | Create three buffers and return the 'BufferRef' of the second buffer
--- created which should be unfoccused.
-createInitialBuffers :: EditorM (BufferRef, String)
+-- | Create three buffers and return the 'BufferRef' and buffer name of
+-- each.
+createInitialBuffers :: EditorM [(BufferRef, BufferName)]
 createInitialBuffers = do
-    _   <- newBufferE (Right "one")   (fromString "Buffer one")
+    one   <- newBufferE (Right "one")   (fromString "Buffer one")
     two   <- newBufferE (Right "two")   (fromString "Buffer two")
-    _ <- newBufferE (Right "three") (fromString "Buffer three")
-    return (two, "two")
+    three <- newBufferE (Right "three") (fromString "Buffer three")
+    return [(one, "one"), (two, "two"), (three, "three")]
+
+
+nthBufferRef :: Int -> [(BufferRef, BufferName)] -> BufferRef
+nthBufferRef n buffers = fst $ buffers !! n
+
+nthBufferName :: Int -> [(BufferRef, BufferName)] -> BufferName
+nthBufferName n buffers = snd $ buffers !! n
+
 
 tests :: Config -> KeyEval -> TestTree
 tests c ev =
@@ -27,15 +36,15 @@ tests c ev =
         testCase ":buffer {bufname} switches to the named buffer" $ do
             let setupActions = createInitialBuffers
 
-                preConditions editor (bufferKey, _name) =
-                    assertNotCurrentBuffer bufferKey editor
+                preConditions editor buffers =
+                    assertNotCurrentBuffer (nthBufferRef 1 buffers) editor
 
-                testActions (_, name) =
-                    ev $ ":buffer " ++ name ++ "<CR>"
+                testActions buffers =
+                    ev $ ":buffer " ++ nthBufferName 1 buffers ++ "<CR>"
 
-                assertions editor (bufferKey, _) = do
+                assertions editor buffers = do
                     assertContentOfCurrentBuffer c "Buffer two" editor
-                    assertCurrentBuffer bufferKey editor
+                    assertCurrentBuffer (nthBufferRef 1 buffers) editor
 
             runTest setupActions preConditions testActions assertions c
 
@@ -43,15 +52,16 @@ tests c ev =
       , testCase ":buffer N switches to the numbered buffer" $ do
             let setupActions = createInitialBuffers
 
-                preConditions editor (bufferKey, _) =
-                    assertNotCurrentBuffer bufferKey editor
+                preConditions editor buffers =
+                    assertNotCurrentBuffer (nthBufferRef 1 buffers) editor
 
-                testActions (BufferRef bref, _) =
-                    ev $ ":buffer " ++ show bref ++ "<CR>"
+                testActions buffers =
+                    let (BufferRef bref) = nthBufferRef 1 buffers
+                    in ev $ ":buffer " ++ show bref ++ "<CR>"
 
-                assertions editor (bufferKey, _) = do
+                assertions editor buffers = do
                     assertContentOfCurrentBuffer c "Buffer two" editor
-                    assertCurrentBuffer bufferKey editor
+                    assertCurrentBuffer (nthBufferRef 1 buffers) editor
 
             runTest setupActions preConditions testActions assertions c
     ]
