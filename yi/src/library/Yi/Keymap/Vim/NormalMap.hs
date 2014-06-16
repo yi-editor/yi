dcd843d3ef01499d634c7cc2c52a24e157db9484
Jared Hance <jared.hance@case.edu>
2014-06-15 20:02:15 -0400

    Add Ctrl-] keybinding for tag jump.

--- yi/src/library/Yi/Keymap/Vim/NormalMap.hs
+++ yi/src/library/Yi/Keymap/Vim/NormalMap.hs
@@ -27,6 +27,7 @@ import Yi.Keymap.Vim.Search
 import Yi.Keymap.Vim.StateUtils
 import Yi.Keymap.Vim.StyledRegion
 import Yi.Keymap.Vim.Utils
+import Yi.Keymap.Vim.Tag
 import Yi.MiniBuffer
 import Yi.Misc
 import Yi.Regex (seInput, makeSearchOptsM)
@@ -54,7 +55,12 @@ defNormalMap operators =
     nonrepeatableBindings ++
     jumpBindings ++
     fileEditBindings ++
-    [tabTraversalBinding]
+    [tabTraversalBinding] ++
+    [tagJumpBinding]
+
+tagJumpBinding :: VimBinding
+tagJumpBinding = mkBindingY Normal (Event (KASCII ']') [MCtrl], f, id)
+   where f = withBuffer readCurrentWordB >>= gotoTag
 
 motionBinding :: VimBinding
 motionBinding = mkMotionBinding Drop $
