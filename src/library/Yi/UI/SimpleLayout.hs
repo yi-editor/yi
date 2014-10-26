f70eda457b4f6275272c892a96973d38ba9387d9
Wayne Warren <waynr@sdf.org>
2014-10-26 10:25:30 -0700

    (#647) Handle cursor positioning edge cases in SimpleLayout
    Signed-off-by: Wayne Warren <waynr@sdf.org>

--- src/library/Yi/UI/SimpleLayout.hs
+++ src/library/Yi/UI/SimpleLayout.hs
@@ -132,14 +132,16 @@ coordsOfCharacterB (Size2D w h) (Point topLeft) (Point char)
 coordsOfCharacterB (Size2D w h) (Point topLeft) (Point char) = savingPointB $ do
     ts <- fmap tabSize indentSettingsB
     text <- fmap (R.toString . R.take (w * h)) (streamB Forward (Point topLeft))
-    let go !x !y n t | x >= w = go (x - w) (y + 1) n t
-        go _  !y _ _ | y >= h = Nothing
+    let go _  !y _ _ | y >= h = Nothing
         go !x !y 0 _ = Just (Point2D x y)
-        go !x !y !n (c : t) =
-            case c of
-                '\t' -> go (x + ts) y (n - 1) t
-                '\n' -> go 0 (y + 1) (n - 1) t
-                _ -> go (x + 1) y (n - 1) t
+        go !x !y !n (c : d : t) =
+            case (c, d, (compare x wOffset)) of
+                ('\t',  _ , _) -> go (x + ts) y (n - 1) (d:t)
+                ('\n',  _ , _) -> go 0 (y + 1) (n - 1) (d:t)
+                (  _ ,'\n',EQ) -> go x y (n - 1) (d:t)
+                (  _ ,  _ ,EQ) -> go (x - wOffset) (y + 1) (n - 1) (d:t)
+                (  _ ,  _ , _) -> go (x + 1) y (n - 1) (d:t)
+            where wOffset = w - 1
         go !x !y _ _ = Just (Point2D x y)
     return (go 0 0 (char - topLeft) text)
 
