57f69ae48ce6345a2cfd69c7a35c5edaa797432e
Michal J. Gajda <mjgajda@gmail.com>
2014-11-07 08:05:17 +0800

    Vim bindings for changing layout managers.

--- src/library/Yi/Keymap/Vim/NormalMap.hs
+++ src/library/Yi/Keymap/Vim/NormalMap.hs
@@ -311,6 +311,11 @@ nonrepeatableBindings = fmap (mkBindingE Normal Drop)
     , ("<C-w>p", prevWinE, resetCount)
     , ("<C-w><Up>", prevWinE, resetCount) -- TODO: please implement upWinE
     , ("<C-w><Left>", prevWinE, resetCount) -- TODO: please implement leftWinE
+    , ("<C-w>l", layoutManagersNextE, resetCount)
+    , ("<C-w>L", layoutManagersPreviousE, resetCount)
+    --, ("<C-w> ", layoutManagersNextE, resetCount)
+    , ("<C-w>v", layoutManagerNextVariantE, resetCount)
+    , ("<C-w>V", layoutManagerPreviousVariantE, resetCount)
     , ("<C-a>", getCountE >>= withCurrentBuffer . incrementNextNumberByB, resetCount)
     , ("<C-x>", getCountE >>= withCurrentBuffer . incrementNextNumberByB . negate, resetCount)
 
