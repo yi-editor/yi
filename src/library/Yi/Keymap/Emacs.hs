2d8df26d5bace271df31cdb5288e1c2538ed529f
Mateusz Kowalczyk <fuuzetsu@fuuzetsu.co.uk>
2015-05-02 02:18:42 +0100

    Style only

--- src/library/Yi/Keymap/Emacs.hs
+++ src/library/Yi/Keymap/Emacs.hs
@@ -94,10 +94,12 @@ deleteB' = adjBlock (-1) >> deleteN 1
 -- | Wrapper around 'moveE' which also cancels incremental search. See
 -- issue #499 for details.
 moveE :: TextUnit -> Direction -> EditorM ()
-moveE u d = getRegexE >>= (\case -- let's check whether searching is in progress (issues #738, #610)
-                               Nothing -> return ()
-                               _ -> isearchFinishWithE resetRegexE)
-                      >> withCurrentBuffer (moveB u d)
+moveE u d = do
+  getRegexE >>= \case
+    -- let's check whether searching is in progress (issues #738, #610)
+    Nothing -> return ()
+    _ -> isearchFinishWithE resetRegexE
+  withCurrentBuffer (moveB u d)
 
 emacsKeys :: Maybe Int -> Keymap
 emacsKeys univArg =
