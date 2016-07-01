a2ba02d9cdfa442c235b7793353a3f63dd5ccdbb
Dmitry Ivanov <ethercrow@gmail.com>
2016-07-01 21:36:14 +0600

    Split keymaps

--- yi-core/src/Yi/Main.hs
+++ yi-core/src/Yi/Main.hs
@@ -69,9 +69,7 @@ data Opts = Help
 
 -- | List of editors for which we provide an emulation.
 editors :: [(String,Config -> Config)]
-editors = [("emacs", toEmacsStyleConfig),
-           ("vim",  toVimStyleConfig),
-           ("cua",   toCuaStyleConfig)]
+editors = []
 
 options :: [OptDescr Opts]
 options =
