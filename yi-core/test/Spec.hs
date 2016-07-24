5c9a5b7a8174f5d600a563c5573b28d3e0d16694
Jared Hance <jaredhance@gmail.com>
2016-07-24 14:29:20 -0700

    Move yi-core tests to hunit

--- yi-core/test/Spec.hs
+++ yi-core/test/Spec.hs
@@ -1,5 +1,5 @@
 import Test.Tasty
-import Test.Tasty.Hspec
+import Test.Tasty.HUnit
 import Test.Tasty.QuickCheck
 
 import qualified Yi.CompletionTree as CT
@@ -7,13 +7,19 @@ import           Data.List         (nub, sort)
 import qualified Data.Map          as M
 
 main :: IO ()
-main = defaultMain . tests =<< specTests
+main = defaultMain tests
 
-tests :: TestTree -> TestTree
-tests specTests' = testGroup "CompletionTree Tests" [properties, specTests']
+tests :: TestTree
+tests = testGroup "all" [completionTreeTests]
 
-properties :: TestTree
-properties = testGroup "(checked by QuickCheck)"
+completionTreeTests :: TestTree
+completionTreeTests = testGroup "CompletionTree"
+    [ completionTreePropertyTests
+    , completionTreeUnitTests
+    ]
+
+completionTreePropertyTests :: TestTree
+completionTreePropertyTests = testGroup "properties"
   [ testProperty "CT.toList . CT.fromList == nub . sort" $
       \list -> CT.toList (CT.fromList list) == sort (nub (list :: [String]))
   , testProperty "update (fromList [a]) a == fromList [mempty] (a is a non-empty string)" $
@@ -24,26 +30,29 @@ properties = testGroup "(checked by QuickCheck)"
       \string -> CT.complete (CT.fromList [string]) == (string,CT.fromList [""])
   ]
 
-specTests :: IO TestTree
-specTests = testSpec "Spec tests" $
-  describe "CompletionTree" $ do
-    describe "fromList" $ do
-      it "returns an empty CompletionTree when given an empty list" $
-        CT.fromList [] `shouldBe` (mempty :: CT.CompletionTree String)
-      it "returns a map with one key when given a list with one item" $
-        CT.fromList ["a"] `shouldBe` CT.CompletionTree (M.fromList [("a",mempty)])
-      it "groups elements with the same prefix" $
-        CT.fromList ["aa","ab"] `shouldBe` CT.CompletionTree (M.fromList [("a",CT.CompletionTree $ M.fromList [("a",mempty),("b",mempty)])])
+completionTreeUnitTests :: TestTree
+completionTreeUnitTests = testGroup "unit tests"
+  [ testGroup "fromList"
+      [ testCase "returns an empty CompletionTree when given an empty list" $
+          CT.fromList [] @?= (mempty :: CT.CompletionTree String)
+      , testCase "returns a map with one key when given a list with one item" $
+          CT.fromList ["a"] @?= CT.CompletionTree (M.fromList [("a",mempty)])
+      , testCase "groups elements with the same prefix" $
+          CT.fromList ["aa","ab"] @?= CT.CompletionTree (M.fromList [("a",CT.CompletionTree $ M.fromList [("a",mempty),("b",mempty)])])
+      ]
+  , testGroup "update"
     -- toList is covered by the SmallCheck and QuickCheck
-    describe "update" $ do
-      it "strips its argument from a matching key" $
-        CT.update (CT.fromList ["abc"]) "a" `shouldBe` CT.fromList ["bc"]
-      it "descends the tree if a substring of its input is found in the CompletionTree" $
-        CT.update (CT.fromList ["put","putStr"]) "putS" `shouldBe` CT.fromList ["tr"]
-      it "returns an empty list if it can't find a matching key" $
-        CT.update (CT.fromList ["put"]) "list" `shouldBe` CT.fromList []
-    describe "complete" $ do
-      it "Returns the common prefix" $
-        CT.complete (CT.fromList ["put","putStr","putStrLn"]) == ("put",CT.fromList ["","Str","StrLn"])
-      it "Returns an empty string if there's no common prefix" $
-        CT.complete (CT.fromList ["put","putStr","abc"]) == ("",CT.fromList ["put","putStr","abc"])
+      [ testCase "strips its argument from a matching key" $
+          CT.update (CT.fromList ["abc"]) "a" @?= CT.fromList ["bc"]
+      , testCase "descends the tree if a substring of its input is found in the CompletionTree" $
+          CT.update (CT.fromList ["put","putStr"]) "putS" @?= CT.fromList ["tr"]
+      , testCase "returns an empty list if it can't find a matching key" $
+          CT.update (CT.fromList ["put"]) "list" @?= CT.fromList []
+      ]
+  , testGroup "complete"
+      [ testCase "Returns the common prefix" $
+        CT.complete (CT.fromList ["put","putStr","putStrLn"]) @?= ("put",CT.fromList ["","Str","StrLn"])
+      , testCase "Returns an empty string if there's no common prefix" $
+        CT.complete (CT.fromList ["put","putStr","abc"]) @?= ("",CT.fromList ["put","putStr","abc"])
+      ]
+  ]
