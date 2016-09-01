2e05f86c291688f2b01b8818fa1b46440f684faf
Hi-Angel <Hi-Angel@yandex.ru>
2016-09-01 18:18:06 +0300

    Explanation of what are "static" configs worth for, and how to use.

--- example-configs/yi-emacs-vty-static/Main.hs
+++ example-configs/yi-emacs-vty-static/Main.hs
@@ -1,3 +1,11 @@
+-- This version of config have to be rebuilt manually, as opposed to dynamic ones, where building is
+-- handled by dyre. An example of building via stack:
+--
+-- 1. copy everything to ~/.config/yi/ directory
+-- 2. change in lts.yaml and stack.yaml files the "location" so that it'd point to the root of the
+--    Yi sources directory (by default its value meant to be built straight in the source)
+-- 3. run "stack install"
+-- After that you can run the binary with "stack exec yi" (or just "yi" if you configured $PATH acc.)
 
 import Lens.Micro.Platform
 import Control.Monad.State.Lazy
