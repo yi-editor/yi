8fe18c77d26af9c56d2ffbc1e1a7911ae7321983
Dmitry Ivanov <ethercrow@gmail.com>
2016-10-03 00:10:43 +0700

    Bring yi package to a more usable state

--- yi/src/Main.hs
+++ yi/src/Main.hs
@@ -2,8 +2,26 @@
 
 module Main (main) where
 
+import Control.Monad.State
+
 import Yi.Boot (yiDriver')
 import Yi.Config.Default (defaultConfig)
 
+import Yi.Config.Default.Vty
+import Yi.Config.Default.Emacs
+import Yi.Config.Default.HaskellMode
+import Yi.Config.Default.JavaScriptMode
+import Yi.Config.Default.MiscModes
+import Yi.Config.Simple.Types
+
 main :: IO ()
-main = yiDriver' True defaultConfig
+main = do
+    cfg <- execStateT configure defaultConfig
+    yiDriver' True cfg
+    where
+    configure = runConfigM $ do
+        configureVty
+        configureEmacs
+        configureHaskellMode
+        configureJavaScriptMode
+        configureMiscModes
