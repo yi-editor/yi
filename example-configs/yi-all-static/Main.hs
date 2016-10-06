967d80be1287930e824a5c927d67a0d5f8d9700d
Jaro Reinders <noughtmare@openmailbox.org>
2016-10-06 10:02:39 +0200

    Improve readability and add line number functionality

--- example-configs/yi-all-static/Main.hs
+++ example-configs/yi-all-static/Main.hs
@@ -5,20 +5,25 @@
 -- Here's a building example with "stack":
 -- 1. Edit "stack.yaml" file so that "location: " would point to the root of the Yi source code
 -- 2. Run "stack install"
+--  (You can add "--flag yi-all-static:-vty" for example to leave out the vty
+--  frontend, see the package.yaml file for all available flags)
 -- 3. Run the built with "stack exec yi" (or just "yi" if you have $PATH
 --    configured acc.)
+--    (Please look at the command line options with "stack exec yi -- -h")
 -- The final name of the executable can be changed in the "package.yaml" file.
 
-import Control.Monad.State.Lazy
-import Data.List
-import Lens.Micro.Platform
-import System.Environment
+import Control.Monad.State.Lazy (execStateT)
+import Data.List                (intersperse)
+import Lens.Micro.Platform      ((.=))
+import Data.Maybe               (fromMaybe)
+
 import Options.Applicative
 
 import Yi hiding (option)
 import Yi.Config.Simple.Types
+import Yi.Buffer.Misc (lineMoveRel)
 import Yi.Config.Default.HaskellMode (configureHaskellMode)
-import Yi.Config.Default.MiscModes (configureMiscModes)
+import Yi.Config.Default.MiscModes   (configureMiscModes)
 
 #ifdef VIM
 import Yi.Config.Default.Vim (configureVim)
@@ -33,10 +38,32 @@ import Yi.Config.Default.Emacs (configureEmacs)
 import Yi.Config.Default.Pango (configurePango)
 #endif
 
+frontends :: [(String, ConfigM ())]
+frontends = [
+#ifdef PANGO
+  ("pango", configurePango),
+#endif
+#ifdef VTY
+  ("vty", configureVty),
+#endif
+  ("", return ())
+  ]
+
+keymaps :: [(String, ConfigM ())]
+keymaps = [
+#ifdef EMACS
+  ("emacs", configureEmacs),
+#endif
+#ifdef VIM
+  ("vim", configureVim),
+#endif
+  ("", return ())
+  ]
+
 data CommandLineOptions = CommandLineOptions {
     frontend :: Maybe String
   , keymap :: Maybe String
-  , startOnLine :: Maybe Integer
+  , startOnLine :: Maybe Int
   , files :: [String]
   }
 
@@ -50,17 +77,17 @@ commandLineOptions = flag' Nothing
         ( long "frontend"
        <> short 'f'
        <> metavar "FRONTEND"
-       <> help "The frontend to use"))
+       <> help "The frontend to use (default is pango)"))
     <*> optional (strOption
         ( long "keymap"
        <> short 'k'
        <> metavar "KEYMAP"
-       <> help "The keymap to use"))
+       <> help "The keymap to use (default is emacs)"))
     <*> optional (option auto
         ( long "line"
        <> short 'l'
        <> metavar "NUM"
-       <> help "Open the file on line NUM (not yet working)"))
+       <> help "Open the (last) file on line NUM"))
     <*> many (argument str (metavar "FILES..."))
   ))
 
@@ -71,45 +98,27 @@ main = do
       Nothing -> putStrLn "Yi 0.13.0.1"
       Just clo -> do
         let openFileActions = intersperse (EditorA newTabE) (map (YiA . openNewFile) (files clo))
+            moveLineAction  = YiA $ withCurrentBuffer (lineMoveRel (fromMaybe 0 (startOnLine clo)))
         cfg <- execStateT
-            (runConfigM (myConfig (frontend clo) (keymap clo) >> (startActionsA .= openFileActions)))
+            (runConfigM (myConfig (frontend clo) (keymap clo) >> (startActionsA .= (openFileActions ++ [moveLineAction]))))
             defaultConfig
         startEditor cfg Nothing
   where 
    opts = info (helper <*> commandLineOptions)
      ( fullDesc
     <> progDesc "Edit files"
-    <> header "yi - a flexible extensible text editor written in haskell")
+    <> header "Yi - a flexible and extensible text editor written in haskell")
 
 myConfig :: Maybe String -> Maybe String -> ConfigM ()
 myConfig f k = do
-#ifdef PANGO
-  if f == Just "pango" || f == Nothing
-    then configurePango
-    else return ()
-#endif
-#ifdef VTY
-#ifndef PANGO
-  if f == Just "vty" || f == Nothing
-#else
-  if f == Just "vty"
-#endif
-    then configureVty
-    else return ()
-#endif
-#ifdef EMACS
-  if k == Just "emacs" || k == Nothing
-    then configureEmacs
-    else return ()
-#endif
-#ifdef VIM
-#ifndef EMACS
-  if k == Just "vim" || k == Nothing
-#else
-  if k == Just "vim"
-#endif
-    then configureVim
-    else return ()
-#endif
+  -- Lookup f in the frontends list or pick the first element of the frontends list if
+  -- f is nothing or do nothing if f is not found in the frontends list.
+  case f of
+    Nothing -> snd (head frontends)
+    Just f' -> fromMaybe (return ()) (lookup f' frontends)
+  -- Same as above, but then with k and keymaps
+  case k of
+    Nothing -> snd (head keymaps)
+    Just k' -> fromMaybe (return ()) (lookup k' keymaps)
   configureHaskellMode
   configureMiscModes
