89600505583752e6911563db4d1c4a7ac11b5040
Jan Path <mail@jpath.de>
2017-03-21 20:21:19 +0100

    Support all abbreviations for :nohlsearch

--- yi-keymap-vim/src/Yi/Keymap/Vim/Ex/Commands/Nohl.hs
+++ yi-keymap-vim/src/Yi/Keymap/Vim/Ex/Commands/Nohl.hs
@@ -10,17 +10,17 @@
 
 module Yi.Keymap.Vim.Ex.Commands.Nohl (parse) where
 
-import           Data.Text                        ()
+import qualified Data.Text                        as T
 import           Yi.Keymap                        (Action (EditorA))
-import           Yi.Keymap.Vim.Common             (EventString)
+import           Yi.Keymap.Vim.Common             (EventString(..))
 import           Yi.Keymap.Vim.Ex.Commands.Common (pureExCommand)
 import           Yi.Keymap.Vim.Ex.Types           (ExCommand (cmdAction, cmdShow))
 import           Yi.Search                        (resetRegexE)
 
 parse :: EventString -> Maybe ExCommand
-parse s = if s == "nohl" || s == "nohlsearch"
-    then Just nohl
-    else Nothing
+parse (Ev s)
+  | T.isPrefixOf s "nohlsearch" && T.compareLength s 2 == GT = Just nohl
+  | otherwise                                                = Nothing
 
 nohl :: ExCommand
 nohl = pureExCommand {
