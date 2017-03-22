13c6bc3e1b7c7e786c789f60b7cab602a8d0ad38
Jan Path <mail@jpath.de>
2017-03-22 18:16:22 +0100

    :d should take a region

--- yi-keymap-vim/src/Yi/Keymap/Vim/Ex/Commands/Delete.hs
+++ yi-keymap-vim/src/Yi/Keymap/Vim/Ex/Commands/Delete.hs
@@ -12,20 +12,24 @@ module Yi.Keymap.Vim.Ex.Commands.Delete (parse) where
 
 import           Control.Applicative              (Alternative ((<|>)))
 import           Control.Monad                    (void)
+import qualified Data.Attoparsec.Text             as P (string, try, match)
+import           Data.Maybe                       (fromMaybe)
 import           Data.Text                        ()
-import qualified Data.Attoparsec.Text             as P (string, try)
+import           Data.Semigroup                   ((<>))
+import           Lens.Micro.Platform
 import           Yi.Buffer.Adjusted               hiding (Delete)
 import           Yi.Keymap                        (Action (BufferA))
 import           Yi.Keymap.Vim.Common             (EventString)
-import qualified Yi.Keymap.Vim.Ex.Commands.Common as Common (parse, pureExCommand)
+import qualified Yi.Keymap.Vim.Ex.Commands.Common as Common (parse, pureExCommand, parseRange)
 import           Yi.Keymap.Vim.Ex.Types           (ExCommand (cmdAction, cmdShow))
 
 parse :: EventString -> Maybe ExCommand
 parse = Common.parse $ do
+    (rangeText, rangeB) <- over _2 (fromMaybe currentLineRegionB) 
+      <$> P.match Common.parseRange
     void $ P.try ( P.string "delete") <|> P.string "d"
     return $ Common.pureExCommand {
-        cmdShow = "delete"
-      , cmdAction = BufferA $ do
-            deleteUnitB Line Forward
-            deleteN 1
+        cmdShow = rangeText <> "delete"
+      , cmdAction = BufferA $ deleteRegionB =<< rangeB
       }
+  where currentLineRegionB = flip convertRegionToStyleB LineWise =<< regionOfB Line
