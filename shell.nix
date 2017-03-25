6bc3e036263b44eda5be2636bfa8c45c5972deda
Mateusz Kowalczyk <fuuzetsu@fuuzetsu.co.uk>
2017-03-25 13:49:11 +0000

    Add which and xsel to nix shell env
    Hclip uses these for for clipboard operations (copy, yank)

--- shell.nix
+++ shell.nix
@@ -17,12 +17,14 @@ stdenv.mkDerivation {
       ncurses.out
       pkgconfig
       glibcLocales
+      which
+      xsel
       zlib.out
   ];
 
   shellHook = ''
     export LC_ALL=en_US.UTF-8
-    export PATH="${haskellPackages.gtk2hs-buildtools}/bin:$PATH"
+    export PATH="${which}/bin:${xsel}/bin:${haskellPackages.gtk2hs-buildtools}/bin:$PATH"
     export CPATH="${icu.dev}/include:${ncurses.dev}/include:$CPATH"
     export LIBRARY_PATH="${icu.dev}/lib:${ncurses.dev}/lib:${zlib.out}/lib:$LIBRARY_PATH"
     export LD_LIBRARY_PATH="${icu.out}/lib:${ncurses.out}/lib:$LD_LIBRARY_PATH"
