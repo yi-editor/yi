71c2d5d7ff06bf2f185862e98e84dcc44f27dc83
Jeremy List <quick.dudley@gmail.com>
2017-07-06 20:19:43 +1200

    Add argument to :new command in vim keymap

--- yi-keymap-vim/src/Yi/Keymap/Vim/Ex/Commands/BufferNew.hs
+++ yi-keymap-vim/src/Yi/Keymap/Vim/Ex/Commands/BufferNew.hs
@@ -10,8 +10,8 @@ module Yi.Keymap.Vim.Ex.Commands.BufferNew (parse) where
 
 import           Control.Applicative              (Alternative(..))
 import           Control.Monad                    (void)
-import qualified Data.Attoparsec.Text as P        (string,try)
-import           Data.Text                        (Text)
+import qualified Data.Attoparsec.Text as P        (anyChar,char,string,try)
+import           Data.Text                        (Text,pack)
 import           Yi.Buffer                        (BufferId (MemBuffer))
 import           Yi.Editor                        (newEmptyBufferE,switchToBufferE)
 import           Yi.Keymap                        (Action (EditorA))
@@ -22,8 +22,10 @@ import           Yi.Keymap.Vim.Ex.Types           (ExCommand (cmdAction, cmdShow
 parse :: EventString -> Maybe ExCommand
 parse = Common.parse $ do
     void $ P.string "new"
+    n <- (some (P.char ' ') *> many (P.anyChar)) <|>
+      ("New Buffer" <$ many (P.char ' '))
     return $ Common.pureExCommand {
         cmdShow = "new"
-      , cmdAction = EditorA $ newEmptyBufferE (MemBuffer "New Buffer") >>=
+      , cmdAction = EditorA $ newEmptyBufferE (MemBuffer $ pack n) >>=
           switchToBufferE
       }
