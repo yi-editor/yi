f18374231818360403e2eadde04036f3ca987d77
Jeremy List <quick.dudley@gmail.com>
2017-07-06 13:05:19 +1200

    Fixed parser for ":!" vim command

--- yi-keymap-vim/src/Yi/Keymap/Vim/Ex/Commands/Shell.hs
+++ yi-keymap-vim/src/Yi/Keymap/Vim/Ex/Commands/Shell.hs
@@ -12,7 +12,7 @@ module Yi.Keymap.Vim.Ex.Commands.Shell (parse) where
 
 import           Control.Monad                    (void)
 import qualified Data.Text                        as T (pack)
-import qualified Data.Attoparsec.Text             as P (char, many1)
+import qualified Data.Attoparsec.Text             as P (char,many1,satisfy)
 import           Yi.Command                       (buildRun)
 import           Yi.Keymap                        (Action (YiA))
 import           Yi.Keymap.Vim.Common             (EventString)
@@ -22,7 +22,7 @@ import           Yi.Keymap.Vim.Ex.Types           (ExCommand (cmdAction, cmdShow
 parse :: EventString -> Maybe ExCommand
 parse = Common.parse $ do
     void $ P.char '!'
-    cmd <- T.pack <$> P.many1 (P.char ' ')
+    cmd <- T.pack <$> P.many1 (P.satisfy  (/=' '))
     args <- Common.commandArgs
     return $ Common.impureExCommand {
         cmdShow = "!"
