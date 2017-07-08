c8d1a791340ded9b30d57f3fff0cf03d7ebf85b0
Jeremy List <quick.dudley@gmail.com>
2017-07-08 23:53:12 +1200

    Use newTempBufferE for :new vim command if no argument

--- yi-keymap-vim/src/Yi/Keymap/Vim/Ex/Commands/BufferNew.hs
+++ yi-keymap-vim/src/Yi/Keymap/Vim/Ex/Commands/BufferNew.hs
@@ -10,22 +10,26 @@ module Yi.Keymap.Vim.Ex.Commands.BufferNew (parse) where
 
 import           Control.Applicative              (Alternative(..))
 import           Control.Monad                    (void)
-import qualified Data.Attoparsec.Text as P        (anyChar,char,string,try)
-import           Data.Text                        (Text,pack)
+import qualified Data.Attoparsec.Text as P        (anyChar, char, string)
+import           Data.List                        (null)
+import           Data.Text as T                   (Text, pack)
 import           Yi.Buffer                        (BufferId (MemBuffer))
-import           Yi.Editor                        (newEmptyBufferE,switchToBufferE)
+import           Yi.Editor                        (newEmptyBufferE, newTempBufferE, switchToBufferE)
 import           Yi.Keymap                        (Action (EditorA))
 import           Yi.Keymap.Vim.Common             (EventString)
-import qualified Yi.Keymap.Vim.Ex.Commands.Common as Common (parse,pureExCommand)
+import qualified Yi.Keymap.Vim.Ex.Commands.Common as Common (parse, pureExCommand)
 import           Yi.Keymap.Vim.Ex.Types           (ExCommand (cmdAction, cmdShow))
 
 parse :: EventString -> Maybe ExCommand
 parse = Common.parse $ do
     void $ P.string "new"
     n <- (some (P.char ' ') *> many (P.anyChar)) <|>
-      ("New Buffer" <$ many (P.char ' '))
+      ("" <$ many (P.char ' '))
     return $ Common.pureExCommand {
         cmdShow = "new"
-      , cmdAction = EditorA $ newEmptyBufferE (MemBuffer $ pack n) >>=
-          switchToBufferE
+      , cmdAction = EditorA $ do
+              b <- if null n
+                  then newTempBufferE
+                  else newEmptyBufferE (MemBuffer $ T.pack n)
+              switchToBufferE b
       }
