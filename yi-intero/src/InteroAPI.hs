8b14004c23de7618d417fa85ae41e5802dc51699
Jaro Reinders <noughtmare@openmailbox.org>
2017-07-29 18:05:48 +0200

    Clean up and document yi-intero

--- yi-intero/src/InteroAPI.hs
+++ yi-intero/src/InteroAPI.hs
@@ -1,105 +1,81 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE FlexibleInstances #-}
-module InteroAPI (Request, Intero (Intero), start, locAt, uses, typeAt, eval) where
 
-import Control.Exception
-import Control.Concurrent.MVar
-import Control.Concurrent
-import System.IO
-import System.Process
-import Control.Monad
+-- |
+-- Module     : InteroAPI
+-- Copyright  : 2017, Jaro Reinders
+-- License    : GPL-2
+-- Maintainer : yi-devel@googlegroups.com
+--
+-- A Haskell API to intero. This is not Yi-specific.
 
--- TODO: remove the duplication
-data Request
-  = Uses   FilePath (Int,Int,Int,Int) String (MVar String)
-  | TypeAt FilePath (Int,Int,Int,Int) String (MVar String)
-  | LocAt  FilePath (Int,Int,Int,Int) String (MVar String)
-  | Eval   String                            (MVar String)
+module InteroAPI (
+    Request
+  , start
+  , locAt, uses, typeAt, eval
+  ) where
 
-type Repl = (String -> IO String)
-newtype Intero = Intero (Maybe (MVar Request))
+import Control.Exception  (IOException, catch, handle)
+import Control.Concurrent (MVar, newEmptyMVar, takeMVar, putMVar, forkIO)
+import System.IO          (hSetBuffering, hGetChar, BufferMode (NoBuffering), hPutStrLn, hReady)
+import System.Process     (runInteractiveProcess)
+import Control.Monad      (void, forever)
 
-instance Show (MVar Request) where
-  show _ = "Intero"
-
-query :: Request -> String
-query (Uses   file (line,col,line',col') name _) =
-  concat [":uses "   , file, " ", unwords (map show [line,col,line',col']), " ", name]
-query (TypeAt file (line,col,line',col') name _) =
-  concat [":type-at ", file, " ", unwords (map show [line,col,line',col']), " ", name]
-query (LocAt  file (line,col,line',col') name _) =
-  concat [":loc-at " , file, " ", unwords (map show [line,col,line',col']), " ", name]
-query (Eval q _) = q
-
-handleReq :: Repl -> Request -> IO ()
-handleReq repl req@(Uses   _ _ _ res) = putMVar res =<< id (repl (query req))
-handleReq repl req@(TypeAt _ _ _ res) = putMVar res =<< id (repl (query req))
-handleReq repl req@(LocAt  _ _ _ res) = putMVar res =<< id (repl (query req))
-handleReq repl req@(Eval   _     res) = putMVar res =<< id (repl (query req))
+-- | The request that are send to the background process.
+data Request = Request
+  String        -- ^ The content of the query.
+  (MVar String) -- ^ The result will be put into this MVar.
 
 -- Code taken from:
 -- https://github.com/commercialhaskell/intero/blob/28609611c9f7c7d63370ce66e8ebb97676a8374e/src/test/Main.hs#L219-L268
+-- Modified to keep the intero process running in the background
+-- and accept commands via an MVar.
 start :: FilePath -> IO (MVar Request)
 start path = do
   req <- newEmptyMVar
-  (inp,out,err,pid) <-
-    catch (runInteractiveProcess
-             "stack"
-             ["ghci","--with-ghc","intero"]
-             (Just path)
-             Nothing)
-          (\(_ :: IOException) ->
-             error "Couldn't launch intero process.")
+  (inp,out,err,pid) <- catch
+    (runInteractiveProcess "stack" ["ghci","--with-ghc","intero"] (Just path) Nothing)
+    (\(_ :: IOException) -> error "Couldn't launch intero process.")
   hSetBuffering inp NoBuffering
   hSetBuffering out NoBuffering
   hSetBuffering err NoBuffering
-  let repl instr = do
-        catch (do
-                 hPutStrLn inp instr
-                 let getReply = do
-                       mc <- catch (fmap Just (hGetChar out))
-                                   (\(_ :: IOException) ->
-                                      return Nothing)
-                       case mc of
-                         Nothing -> hGetAvailable err
-                         Just '\4' -> hGetAvailable err
-                         Just c -> fmap (c:) getReply
-                 getReply)
-              (\(_ :: IOException) -> return "")
+  let repl instr = handle (\(e :: IOException) -> return (show e)) $ do
+        hPutStrLn inp instr
+        let getReply = do
+              mc <- catch (fmap Just (hGetChar out))
+                          (\(_ :: IOException) -> return Nothing)
+              case mc of
+                Nothing -> hGetAvailable err
+                Just '\4' -> hGetAvailable err
+                Just c -> fmap (c:) getReply
+        getReply
   void $ repl ":set prompt \"\\4\""
-  forkIO $ forever $ takeMVar req >>= handleReq repl
+  forkIO $ forever $ do
+    Request query res <- takeMVar req
+    repl query >>= putMVar res
   return req
   where
     hGetAvailable h = do
       available <- catch (hReady h) (\(_ :: IOException) -> return False)
       if available
-        then catch (do
-                      c <- hGetChar h
-                      cs <- hGetAvailable h
-                      return (c:cs))
+        then catch ((:) <$> hGetChar h <*> hGetAvailable h)
                    (\(_ :: IOException) -> return [])
         else return []
 
 uses :: MVar Request -> FilePath -> (Int,Int,Int,Int) -> String -> IO String
-uses req file range name = do
-  result <- newEmptyMVar
-  putMVar req (Uses file range name result)
-  takeMVar result
+uses req file (line,col,line',col') name = eval req $ unwords $ concat
+  [[":uses",file], map show [line,col,line',col'], [name]]
 
 typeAt :: MVar Request -> FilePath -> (Int,Int,Int,Int) -> String -> IO String
-typeAt req file range name = do
-  result <- newEmptyMVar
-  putMVar req (TypeAt file range name result)
-  takeMVar result
+typeAt req file (line,col,line',col') name = eval req $ unwords $ concat
+  [[":type-at",file], map show [line,col,line',col'], [name]]
 
 locAt :: MVar Request -> FilePath -> (Int,Int,Int,Int) -> String -> IO String
-locAt req file range name = do
-  result <- newEmptyMVar
-  putMVar req (LocAt file range name result)
-  takeMVar result
+locAt req file (line,col,line',col') name = eval req $ unwords $ concat
+  [[":loc-at",file], map show [line,col,line',col'], [name]]
 
 eval :: MVar Request -> String -> IO String
 eval req q = do
   result <- newEmptyMVar
-  putMVar req (Eval q result)
+  putMVar req (Request q result)
   takeMVar result
--- yi-intero/src/Yi/Intero.hs
+++ yi-intero/src/Yi/Intero.hs
@@ -1,5 +1,19 @@
 {-# LANGUAGE OverloadedStrings #-}
-module Yi.Intero where
+
+-- |
+-- Module      : Yi.Intero
+-- Description : Intero support for Yi.
+-- Copyright   : 2017 Jaro Reinders
+-- License     : GPL-2
+-- Maintainer  : yi-devel@googlegroups.com
+--
+-- This module implements actual Yi Actions using the hidden InteroAPI module.
+
+module Yi.Intero (
+    Intero
+  , interoStart
+  , interoLocAt, interoTypeAt, interoEval, interoUses
+  ) where
 
 import qualified Yi.Rope as R
 import Control.Concurrent.MVar
@@ -17,10 +31,24 @@ import Yi.Buffer.Normal
 import Yi.Core
 import Yi.Monad
 import Data.Semigroup ((<>))
+import Control.Monad.Trans.Except
+import Control.Monad.Trans.Class (lift)
+import Data.Text (pack)
+import Control.Exception (IOException, try)
 
 import qualified InteroAPI as Intero
-import InteroAPI (Intero (Intero), Request)
+import InteroAPI (Request)
 
+-- | The main type that facilitates the communication with the background intero process.
+-- Should be Nothing when intero is not yet started and Just an MVar that you can use
+-- to make requests if intero is started.
+--
+-- This type is a YiVariable so we can use putEditorDyn when we start intero
+-- and getEditorDyn whenever we need it.
+newtype Intero = Intero { unIntero :: (Maybe (MVar Request)) }
+
+instance Show Intero where
+  show _ = "Intero"
 instance Binary Intero where
   put _ = return ()
   get = return def
@@ -28,47 +56,63 @@ instance Default Intero where
   def = Intero Nothing
 instance YiVariable Intero
 
+-- | Start the Intero background process.
 interoStart :: Action
 interoStart = YiA $ do
   Intero intero <- getEditorDyn :: YiM Intero
   case intero of
-    -- TODO: change "." into actual directory
-    Nothing -> liftBase (Intero.start ".") >>= putEditorDyn . Intero . Just
-    Just _ -> errorEditor "intero-start: Already started"
+    -- FIXME: change "." into the correct directory
+    Nothing -> liftBase (try (Intero.start ".") :: IO (Either IOException (MVar Request)))
+           >>= either (errorEditor . pack . show) (putEditorDyn . Intero . Just)
+    Just _ -> errorEditor "Intero is already running"
 
+-- | Pass a raw string to intero. Intero is like ghci in that you can
+-- evaluate any haskell expression, but you can also use commands like
+-- ":show modules" or ":show imports" like you would use them in ghci.
+-- On top of all that Intero has implemented ide-like commands, like
+-- ":type-at" and ":uses". Intero's github page has a small list of
+-- features:
+-- https://github.com/commercialhaskell/intero/blob/28271d50ca65c460cd0983cea13a2c4509b95583/TOOLING.md
 interoEval :: String -> Action
-interoEval command = YiA $ do
-  Intero intero <- getEditorDyn :: YiM Intero
-  case intero of
-    Nothing -> errorEditor "intero-eval: Intero not initialized"
-    Just req -> do
-      res <- liftBase $ Intero.eval req command
-      inNewBuffer "*intero*" (R.fromString res)
+interoEval command = YiA $
+  either errorEditor return =<< runExceptT (do
+    intero <- ExceptT $ maybe (Left "Intero not running") Right . unIntero <$> getEditorDyn
+    res    <- liftBase $ Intero.eval intero command
+    -- FIXME: close previous *intero* buffers before opening a new one.
+    lift $ inNewBuffer "*intero*" (R.fromString res))
 
+-- TODO: add possibility of using the current selection as input string to the intero command
+
+-- | Intero's 'loc-at', 'type-at' and 'uses' commands all use the same file-range-name format
+-- so I wrote a function to reduce code duplication.
 interoFileRangeNameAction
-  :: Text
-  -> (MVar Request -> String -> (Int,Int,Int,Int) -> String -> IO String)
+  :: (MVar Request -> String -> (Int,Int,Int,Int) -> String -> IO String)
   -> Action
-interoFileRangeNameAction name f = YiA $ do
-  mayFile <- withCurrentBuffer $ gets file
-  case mayFile of
-    Nothing -> errorEditor (name <> ": Not in file")
-    Just file -> do
-      Intero mayIntero <- getEditorDyn :: YiM Intero
-      case mayIntero of
-        Nothing -> errorEditor (name <> ": Intero not initialized")
-        Just intero -> do
-          region <- withCurrentBuffer $ regionOfB unitWord
-          range <- regionToRange region
-          name <- withCurrentBuffer $ readRegionB region
-          res <- liftBase $ f intero file range (R.toString name)
-          inNewBuffer "*intero*" (R.fromString res)
+interoFileRangeNameAction f = YiA $
+  either errorEditor return =<< runExceptT (do
+    file   <- ExceptT $ maybe (Left "Not in file") Right <$> withCurrentBuffer (gets file)
+    intero <- ExceptT $ maybe (Left "Intero not running") Right . unIntero <$> getEditorDyn
+    region <- lift $ withCurrentBuffer (regionOfB unitWord)
+    range  <- lift $ regionToRange region
+    name   <- lift $ withCurrentBuffer (readRegionB region)
+    res    <- liftBase (f intero file range (R.toString name))
+    lift $ inNewBuffer "*intero*" (R.fromString res))
 
-interoLocAt,interoUses,interoTypeAt :: Action
-interoLocAt  = interoFileRangeNameAction "intero-loc-at"  Intero.locAt
-interoUses   = interoFileRangeNameAction "intero-uses"    Intero.uses
-interoTypeAt = interoFileRangeNameAction "intero-type-at" Intero.typeAt
+-- | Finds the location of the definition of the current word under the cursor.
+-- This will open the result (just a string containing the module name) in a split window.
+interoLocAt  :: Action
+interoLocAt  = interoFileRangeNameAction Intero.locAt
 
+-- | Finds the places where the current word under the cursor is used if it is a definition
+-- or the place where it is defined if it isn't.
+interoUses   :: Action
+interoUses   = interoFileRangeNameAction Intero.uses
+
+-- | Find the type of the current word under the cursor.
+interoTypeAt :: Action
+interoTypeAt = interoFileRangeNameAction Intero.typeAt
+
+-- | Convert a Yi Region to Intero's range format.
 regionToRange :: Region -> YiM (Int,Int,Int,Int)
 regionToRange region = withCurrentBuffer $ do
   line  <- lineOf $ regionStart region
@@ -77,6 +121,7 @@ regionToRange region = withCurrentBuffer $ do
   col'  <- colOf  $ regionEnd   region
   return (line,col,line',col')
 
+-- | Show a YiString in a split window.
 inNewBuffer :: Text -> R.YiString -> YiM ()
 inNewBuffer bufName content = withEditor $ withOtherWindow $ do
   void $ newBufferE (MemBuffer bufName) $ content
