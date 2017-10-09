deb60a6d9d2ac505561bb13c9bbf655364c84a3c
Jaro Reinders <jaro.reinders@gmail.com>
2017-10-09 18:05:28 +0200

    Show error message when no file name is given

--- yi-keymap-vim/src/Yi/Keymap/Vim/Ex/Commands/Edit.hs
+++ yi-keymap-vim/src/Yi/Keymap/Vim/Ex/Commands/Edit.hs
@@ -15,7 +15,7 @@ module Yi.Keymap.Vim.Ex.Commands.Edit (parse) where
 import           Control.Applicative              (Alternative ((<|>)))
 import           Control.Monad                    (void, when)
 import           Data.Maybe                       (isJust)
-import qualified Data.Text                        as T (Text, append, pack, unpack)
+import qualified Data.Text                        as T (Text, append, pack, unpack, null)
 import qualified Data.Attoparsec.Text             as P (anyChar, many', many1, space, string, try, option)
 import           Yi.Editor                        (MonadEditor (withEditor), newTabE)
 import           Yi.File                          (openNewFile)
@@ -23,6 +23,7 @@ import           Yi.Keymap                        (Action (YiA))
 import           Yi.Keymap.Vim.Common             (EventString)
 import qualified Yi.Keymap.Vim.Ex.Commands.Common as Common (filenameComplete, impureExCommand, parse)
 import           Yi.Keymap.Vim.Ex.Types           (ExCommand (cmdAction, cmdComplete, cmdShow))
+import           Yi.Editor                        (printMsg)
 
 parse :: EventString -> Maybe ExCommand
 parse = Common.parse $ do
@@ -35,9 +36,12 @@ parse = Common.parse $ do
 edit :: Bool -> T.Text -> ExCommand
 edit tab f = Common.impureExCommand {
     cmdShow = showEdit tab f
-  , cmdAction = YiA $ do
-        when tab $ withEditor newTabE
-        openNewFile $ T.unpack f
+  , cmdAction = YiA $
+        if T.null f
+          then printMsg "No file name"
+          else do
+            when tab $ withEditor newTabE
+            openNewFile $ T.unpack f
   , cmdComplete = (fmap . fmap)
                     (showEdit tab) (Common.filenameComplete f)
   }
