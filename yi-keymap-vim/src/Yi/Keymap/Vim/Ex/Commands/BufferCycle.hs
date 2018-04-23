3c2390aff7ef45a926e4bd4cbcd7608002e0ebbb
Jeremy List <quick.dudley@gmail.com>
2018-04-23 17:55:28 +1200

    Add :next and :previous commands (ignoring difference between :next and :bnext, etc for now)

---
+++ yi-keymap-vim/src/Yi/Keymap/Vim/Ex/Commands/BufferCycle.hs
@@ -0,0 +1,28 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Yi.Keymap.Vim.Ex.Commands.BufferCycle (parse) where
+
+import           Control.Applicative                 (Alternative ((<|>)))
+import           Control.Monad
+import           Data.List
+import qualified Data.Attoparsec.Text             as P
+import           Lens.Micro.Platform
+import           Yi.Editor
+import           Yi.Keymap                        (Action (EditorA))
+import           Yi.Window
+import           Yi.Keymap.Vim.Common                (EventString)
+import qualified Yi.Keymap.Vim.Ex.Commands.Common as Common
+import           Yi.Keymap.Vim.Ex.Types           (ExCommand (cmdAction, cmdShow))
+
+parse :: EventString -> Maybe ExCommand
+parse = Common.parse $ do
+  void (P.char 'b') <|> return ()
+  direction <- (fmap (const False) $ P.string "prev" <|> P.string "previous") <|> fmap (const True) (P.string "next")
+  return $ Common.pureExCommand {
+    cmdShow = if direction then "next" else "previous",
+    cmdAction = EditorA $ currentWindowA %= \window -> let
+      cl = bufkey window : bufAccessList window
+      cl' = if direction
+        then tail cl ++ [head cl]
+        else last cl : init cl
+      in window { bufkey = head cl', bufAccessList = tail cl' }
+   }
